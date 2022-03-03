a5b7f3de9f739f7b772d931f9eedc5b5d8a16ef3
rewine <1758075541@qq.com>
2022-03-03 16:59:55 +0800

    hello-nur: remove from dde-overlay

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 {
-  hello-nur = pkgs.callPackage ./pkgs/hello-nur {};
   dtkcommon = pkgs.qt5.callPackage ./pkgs/dtkcommon {};
   deepin-desktop-base = pkgs.callPackage ./pkgs/deepin-desktop-base {};
 }
