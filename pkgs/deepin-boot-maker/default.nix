3be1c9d7514547f7115a7928e80c8d9ebd3aa7a3
rewine <1758075541@qq.com>
2022-03-23 16:23:46 +0800

    deepin-boot-maker: init at 5.7.6

---
+++ pkgs/deepin-boot-maker/default.nix
@@ -0,0 +1,63 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, qmake
+, pkgconfig
+, qtbase
+, qttools
+, qtx11extras
+, wrapQtAppsHook
+, qt5integration
+,    mtools
+,    p7zip
+,    syslinux
+, gtest
+, dtkcore
+, dtkgui
+, dtkwidget
+, dtkcommon
+}:
+
+stdenv.mkDerivation rec {
+  pname = "deepin-boot-maker";
+  version = "5.7.6";
+
+  src = fetchFromGitHub {
+    owner = "linuxdeepin";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-UpbU1mFQGCNnf2jusa6Q5wJyMzOQLa1z0txuSqxJRH0=";
+  };
+
+  nativeBuildInputs = [ qmake pkgconfig wrapQtAppsHook ];
+
+  buildInputs = [
+    dtkcore
+    dtkgui
+    dtkwidget
+    dtkcommon
+    mtools
+    p7zip
+    syslinux
+    gtest
+    qttools
+    qtx11extras
+    qt5integration
+  ];
+
+  qmakeFlags = [
+    "PREFIX=${placeholder "out"}"
+  ];
+
+  postPatch = ''
+    substituteInPlace src/src.pro --replace "/usr/share/deepin-boot-maker/translations" "$out/share/deepin-boot-maker/translations"
+    substituteInPlace deepin-boot-maker.pro --replace "/usr/share/deepin-manual/manual-assets/application" "$out/share/deepin-manual/manual-assets/application"
+  '';
+
+  meta = with lib; {
+    description = "Tool to create a bootable usb stick quick and easy";
+    homepage = "https://github.com/linuxdeepin/deepin-boot-maker";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
