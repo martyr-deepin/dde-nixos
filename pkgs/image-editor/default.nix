10ecb022e7f962af4db8c5745bd9f8593c74e2a2
rewine <1758075541@qq.com>
2022-03-24 20:12:31 +0800

    image-editor: init at unstable-2022-03-16

---
+++ pkgs/image-editor/default.nix
@@ -0,0 +1,57 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, fetchpatch
+, dtkcore
+, dtkgui
+, dtkwidget
+, dtkcommon
+, cmake
+, qttools
+, pkgconfig
+, wrapQtAppsHook
+, opencv
+, freeimage
+}:
+
+stdenv.mkDerivation rec {
+  pname = "image-editor";
+  version = "unstable-2022-03-16";
+
+  src = fetchFromGitHub {
+    owner = "linuxdeepin";
+    repo = pname;
+    rev = "29ed6f857449a4bfba6621750eb39a3265c8ad24";
+    sha256 = "sha256-6NRjSkOOGIYghSoibf8moJKRMiepytHk+pn+zjD04mU=";
+  };
+
+  nativeBuildInputs = [ cmake pkgconfig qttools wrapQtAppsHook ];
+
+  buildInputs = [
+    dtkcommon
+    dtkcore
+    dtkgui
+    dtkwidget
+    opencv
+    freeimage
+  ];
+
+  postPatch = ''
+    substituteInPlace libimageviewer/CMakeLists.txt \
+      --replace "set(PREFIX /usr)" "set(PREFIX $out)"
+    substituteInPlace libimagevisualresult/CMakeLists.txt \
+      --replace "set(PREFIX /usr)" "set(PREFIX $out)"
+  '';
+
+  # fix bug https://github.com/linuxdeepin/developer-center/issues/2234
+  postInstall = ''
+    cp -r  $out/include/libimageviewer/* $out/include/
+  '';
+
+  meta = with lib; {
+    description = "image editor lib for dtk";
+    homepage = "https://github.com/linuxdeepin/image-editor";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
