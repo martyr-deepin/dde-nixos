99344337d4b8f96a19be8b8b80bcad63dbf37a69
rewine <1758075541@qq.com>
2022-04-07 17:30:48 +0800

    deepin-wallpapers: init at 1.7.10

---
+++ pkgs/deepin-wallpapers/default.nix
@@ -0,0 +1,41 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, dde-api
+}:
+
+stdenv.mkDerivation rec {
+  pname = "deepin-wallpapers";
+  version = "1.7.10";
+
+  src = fetchFromGitHub {
+    owner = "linuxdeepin";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-D7DXyPh74VlYn5vKUzDh/P/xoskxs8N/BEg5ZemXRwk=";
+  };
+
+  nativeBuildInputs = [ dde-api ];
+
+  postPatch = ''
+    patchShebangs blur_image.sh
+
+    substituteInPlace blur_image.sh \
+      --replace /usr/lib/deepin-api/image-blur ${dde-api}/lib/deepin-api/image-blur
+  '';
+
+  installPhase = ''
+    mkdir -p $out/share/wallpapers/deepin
+    cp deepin/* $out/share/wallpapers/deepin
+
+    mkdir -p $out/share/wallpapers/image-blur
+    cp image-blur/* $out/share/wallpapers/image-blur
+  '';
+
+  meta = with lib; {
+    description = "deepin-wallpapers provides wallpapers of dde";
+    homepage = "https://github.com/linuxdeepin/deepin-wallpapers";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
