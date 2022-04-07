8cd80c574ad2001fd4b60a454e9bf97444a4bb40
rewine <1758075541@qq.com>
2022-04-07 18:12:15 +0800

    deepin-desktop-schemas: init at 5.10.6

---
+++ pkgs/deepin-desktop-schemas/default.nix
@@ -0,0 +1,44 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, buildGoPackage
+, go
+, go-lib
+}:
+
+buildGoPackage rec {
+  pname = "deepin-desktop-schemas";
+  version = "5.10.6";
+
+  goPackagePath = "github.com/linuxdeepin/deepin-desktop-schemas";
+
+  src = fetchFromGitHub {
+    owner = "linuxdeepin";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-fCI9u0eotgaay/Ci8/3bzr4MZ95GN5fH0LBXFKHJlF8=";
+  };
+
+  buildInputs = [ go-lib ];
+
+  preBuild = ''
+    cp -r ${go-lib}/share/gocode/* go/
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+    make ARCH=${stdenv.targetPlatform.linuxArch} -C go/src/${goPackagePath}
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    make install DESTDIR="$out" PREFIX="/" -C go/src/${goPackagePath}
+  '';
+
+  meta = with lib; {
+    description = "GSettings deepin desktop-wide schemas";
+    homepage = "https://github.com/linuxdeepin/deepin-desktop-schemas";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
