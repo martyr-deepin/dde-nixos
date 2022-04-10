576f3e837c05024ec4d2a7cd6593c310a73c0f9d
rewine <1758075541@qq.com>
2022-04-10 23:23:13 +0800

    dde-calendar: init at 8.8.29

--- default.nix
+++ default.nix
@@ -37,10 +37,12 @@ let
     go-gir-generator = callPackage ./pkgs/go-gir-generator { };
     go-lib = callPackage ./pkgs/go-lib { };
     dde-api = callPackage ./pkgs/dde-api { };
-    #lastore-daemon
     deepin-desktop-schemas = callPackage ./pkgs/deepin-desktop-schemas { };
     #dde-daemon
     #startdde
+
+    # Dtk Application
+    dde-calendar = callPackage ./pkgs/dde-calendar { };
 
     # break, need fix
     deepin-camera = callPackage ./pkgs/deepin-camera { };
---
+++ pkgs/dde-calendar/default.nix
@@ -0,0 +1,72 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, dtkcore
+, dtkgui
+, dtkwidget
+, dtkcommon
+, qt5integration
+, qt5platform-plugins
+, dde-qt-dbus-factory
+, cmake
+, qttools
+, pkgconfig
+, wrapQtAppsHook
+, gtest
+}:
+
+stdenv.mkDerivation rec {
+  pname = "dde-calendar";
+  version = "5.8.29";
+
+  src = fetchFromGitHub {
+    owner = "linuxdeepin";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-2J8AvXugOhcsipMvkqJ0SsgIQcXqLe2KgJIDNQC3dzI=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    qttools
+    pkgconfig
+    wrapQtAppsHook
+  ];
+
+  buildInputs = [
+    dtkcommon
+    dtkcore
+    dtkgui
+    dtkwidget
+    dde-qt-dbus-factory
+    gtest
+  ];
+
+  qtWrapperArgs = [
+    "--prefix QT_PLUGIN_PATH : ${qt5integration}/plugins"
+    "--prefix QT_PLUGIN_PATH : ${qt5platform-plugins}/plugins"
+  ];
+  
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace "set(CMAKE_INSTALL_PREFIX /usr)" "set(CMAKE_INSTALL_PREFIX /)" \
+      --replace "ADD_SUBDIRECTORY(calendar-client)" "" \
+      --replace "ADD_SUBDIRECTORY(tests)" ""
+    
+    substituteInPlace calendar-client/CMakeLists.txt \
+      --replace "set(CMAKE_INSTALL_PREFIX /usr)" "set(CMAKE_INSTALL_PREFIX /)" \
+      --replace "/usr/share/deepin-manual/manual-assets/application/)" "share/deepin-manual/manual-assets/application/)"
+
+    substituteInPlace calendar-service/CMakeLists.txt \
+      --replace "set(CMAKE_INSTALL_PREFIX /usr)" "set(CMAKE_INSTALL_PREFIX /)"
+  '';
+
+  installFlags = [ "DESTDIR=$(out)" ];
+
+  meta = with lib; {
+    description = "Calendar for Deepin Desktop Environment";
+    homepage = "https://github.com/linuxdeepin/deepin-calendar";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
