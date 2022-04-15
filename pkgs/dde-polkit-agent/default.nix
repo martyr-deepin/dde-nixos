c9b6e079f4eb7558100107ef1b1cb8546ab77a48
rewine <1758075541@qq.com>
2022-04-15 15:52:48 +0800

    dde-polkit-agent: init at 5.5.7

---
+++ pkgs/dde-polkit-agent/default.nix
@@ -0,0 +1,56 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, dtkcommon
+, dtkcore
+, dtkgui
+, dtkwidget
+, dde-qt-dbus-factory 
+, pkgconfig
+, cmake
+, qttools
+, wrapQtAppsHook
+, polkit-qt-1
+}:
+
+stdenv.mkDerivation rec {
+  pname = "dde-polkit-agent";
+  version = "5.5.7";
+
+  src = fetchFromGitHub {
+    owner = "linuxdeepin";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-zh1tK9jhEvKWyCcriYFOL2ko9E7nA/6Mte9fJL1b+hA=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    pkgconfig
+    qttools
+    wrapQtAppsHook
+  ];
+
+  buildInputs = [
+    dtkcommon
+    dtkcore
+    dtkgui
+    dtkwidget
+    dde-qt-dbus-factory
+    polkit-qt-1
+  ];
+
+  patches = [
+    ./fix_in_non_deepin.patch
+  ];
+
+  postPatch = ''
+  '';
+
+  meta = with lib; {
+    description = "PolicyKit agent for Deepin Desktop Environment";
+    homepage = https://github.com/linuxdeepin/dde-polkit-agent;
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
