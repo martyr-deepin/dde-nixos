9feb685a7f2295a7abbde3d65b92f40075495c7b
rewine <1758075541@qq.com>
2022-04-18 14:00:06 +0800

    dtkwidget: fix translation path

--- pkgs/dtkwidget/default.nix
+++ pkgs/dtkwidget/default.nix
@@ -59,6 +59,13 @@ stdenv.mkDerivation rec {
     "MKSPECS_INSTALL_DIR=${placeholder "out"}/mkspecs"
   ];
 
+  fixTranslationPatch = ''
+    substituteInPlace src/widgets/dapplication.cpp \
+      --replace "auto dataDirs = DStandardPaths::standardLocations(QStandardPaths::GenericDataLocation);" "auto dataDirs = DStandardPaths::standardLocations(QStandardPaths::GenericDataLocation) << \"$out/share\";"
+  '';
+
+  postPatch = fixTranslationPatch;
+
   meta = with lib; {
     description = "Deepin graphical user interface library";
     homepage = "https://github.com/linuxdeepin/dtkwidget";
