b973034a773b0b9ca7501e80c383b01de838728a
rewine <1758075541@qq.com>
2022-04-26 16:49:49 +0800

    deepin-movie-reborn: fix load lib

--- packages/apps/deepin-movie-reborn/default.nix
+++ packages/apps/deepin-movie-reborn/default.nix
@@ -25,8 +25,22 @@
 , gsettings-desktop-schemas
 , wrapGAppsHook
 , gtest
+, libpulseaudio
+, cudaSupport ? false, cudaPackages
 }:
+let
+  replaceLibPath = filePath: ''
+    substituteInPlace ${filePath} \
+      --replace 'libPath("libavcodec.so")'            '"${ffmpeg.out}/lib/libavcodec.so"' \
+      --replace 'libPath("libavformat.so")'           '"${ffmpeg.out}/lib/libavformat.so"' \
+      --replace 'libPath("libavutil.so")'             '"${ffmpeg.out}/lib/libavutil.so"' \
+      --replace 'libPath("libffmpegthumbnailer.so")'  '"${ffmpegthumbnailer.out}/lib/libffmpegthumbnailer.so"' \
+      --replace 'libPath("libmpv.so.1")'              '"${mpv}/lib/libmpv.so.1"'
+  '' + lib.optionalString cudaSupport '' 
+  '';
+  ### TODO src/backends/mpv/mpv_proxy.cpp libgpuinfo.so
 
+in
 stdenv.mkDerivation rec {
   pname = "deepin-movie-reborn";
   version = "5.9.14";
@@ -61,7 +75,9 @@ stdenv.mkDerivation rec {
     mpv
     gsettings-desktop-schemas
     gtest
-  ];
+    xorg.xcbproto
+    libpulseaudio
+  ] ++ lib.optional cudaSupport [ cudaPackages.cudatoolkit ];
 
   patches = [
     ./fix-build.patch
@@ -73,8 +89,14 @@ stdenv.mkDerivation rec {
     "--prefix XDG_DATA_DIRS : ${placeholder "out"}/share/gsettings-schemas/${pname}-${version}"
   ];
 
-  fixCodePatch = ''
+  #makeFlags =  [ "CFLAGS+=-Og" "CFLAGS+=-ggdb" ];
 
+  cmakeFlags = [ 
+    "-DVERSION=${version}"
+    #"-DCMAKE_BUILD_TYPE=Debug"
+  ];
+
+  fixCodePatch = ''
     substituteInPlace src/common/diskcheckthread.cpp \
       --replace "</usr/include/linux/cdrom.h>" "<linux/cdrom.h>"
 
@@ -91,8 +113,21 @@ stdenv.mkDerivation rec {
       --replace "/usr/share/glib-2.0/schemas)" "$out/share/glib-2.0/schemas)" \
       --replace "/usr/share/deepin-manual/manual-assets/application/)" "$out/share/deepin-manual/manual-assets/application/)"
   '';
+  
+  fixLoadLibPatch = lib.concatMapStrings replaceLibPath [
+    "src/libdmr/playlist_model.cpp"
+    "src/libdmr/filefilter.cpp"
+    "src/widgets/platform/platform_toolbox_proxy.cpp"
+    "src/common/platform/platform_thumbnail_worker.cpp"
+    "src/common/thumbnail_worker.cpp"
+    "src/common/hwdec_probe.cpp"
+    "src/widgets/toolbox_proxy.cpp"
+    "src/backends/mpv/mpv_proxy.cpp"
+    "src/backends/mpv/mpv_glwidget.cpp"
+    "src/backends/mpv/mpv_glwidget.cpp"
+  ];
 
-  postPatch = fixCodePatch + fixInstallPatch;
+  postPatch = fixCodePatch + fixInstallPatch + fixLoadLibPatch;
 
   preFixup = ''
     glib-compile-schemas ${glib.makeSchemaPath "$out" "${pname}-${version}"}
