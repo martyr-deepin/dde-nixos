c2d41a5a5c484b1eec486858c0ed9d7371243862
rewine <1758075541@qq.com>
2022-05-05 20:45:08 +0800

    deepin-anything: 5.0.17 -> 6.0.0

--- packages/misc/deepin-anything/default.nix
+++ packages/misc/deepin-anything/default.nix
@@ -7,17 +7,18 @@
 , udisks2-qt5
 , utillinux
 , pcre
+, breakpointHook
 }:
 
 stdenv.mkDerivation rec {
   pname = "deepin-anything";
-  version = "5.0.17";
+  version = "6.0.0";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-OY94ojhGCUxmpOPFgz4phInXntsHI/wLnWeYseMcAi0=";
+    sha256 = "sha256-1GX/gemRTMhP8ZixYzB3mwQrWonjEPNYWY6zutTnLqw=";
   };
 
   output = [ "out" "server" ];
@@ -25,6 +26,7 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [
     qmake
     pkgconfig
+    breakpointHook
   ];
 
   buildInputs = [
@@ -37,18 +39,36 @@ stdenv.mkDerivation rec {
   dontWrapQtApps = true;
   dontUseQmakeConfigure = true;
 
+  # TODO: sysusers
+  fixServerPatch = ''
+    substituteInPlace server/backend/backend.pro \
+      --replace '/usr/share/dbus-1/interfaces' '$server/share/dbus-1/interfaces' \
+      --replace '/etc/dbus-1/system.d' '$server/etc/dbus-2/system.d' \
+      --replace 'INSTALLS += target includes readme' 'INSTALLS += target includes'
+  '';
+
+  postPatch = fixServerPatch;
+
   buildPhase = ''
-    make VERSION=${version}
     make -C library all
     cd server 
-    qmake -makefile -nocache QMAKE_STRIP=: PREFIX=$server LIB_INSTALL_DIR=$server/lib deepin-anything-server.pro 
+    qmake -makefile -nocache QMAKE_STRIP=: PREFIX=$server LIB_INSTALL_DIR=$server/lib deepin-anything-backend.pro 
     make all
     cd ..
   '';
 
   installPhase = ''
-    make VERSION=${version} DESTDIR=$out install
-    # make -C server DESTDIR=$server install
+    mkdir -p $out/lib
+    cp library/bin/release/* $out/lib
+    mkdir -p $out/usr/src/deepin-anything-${version}
+    cp -r kernelmod/* $out/usr/src/deepin-anything-${version}
+    mkdir -p $out/usr/lib/modules-load.d
+    echo "" | tee $out/usr/lib/modules-load.d/anything.conf
+    mkdir -p $out/usr/include/deepin-anything
+    cp -r library/inc/* $out/usr/include/deepin-anything
+    cp -r kernelmod/vfs_change_uapi.h $out/usr/include/deepin-anything
+    cp -r kernelmod/vfs_change_consts.h $out/usr/include/deepin-anything
+    #make -C server install INSTALL_ROOT=$server
   '';
 
   meta = with lib; {
