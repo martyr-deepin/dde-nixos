fee5d0c227c5e084d0b40b5fee3c9d82f4cdfef0
rewine <1758075541@qq.com>
2022-05-16 14:18:21 +0800

    deepin-file-manager: fix gappsWrapperArgs

--- packages/apps/deepin-file-manager/default.nix
+++ packages/apps/deepin-file-manager/default.nix
@@ -182,13 +182,13 @@ stdenv.mkDerivation rec {
   ];
 
   qtWrapperArgs = [
-    "\${gappsWrapperArgs[@]}"
     "--prefix QT_PLUGIN_PATH : ${qt5integration}/plugins"
     "--prefix QT_QPA_PLATFORM_PLUGIN_PATH : ${qt5platform-plugins}/plugins"
   ];
 
   preFixup = ''
     glib-compile-schemas ${glib.makeSchemaPath "$out" "${pname}-${version}"}
+    qtWrapperArgs+=("''${gappsWrapperArgs[@]}")
   '';
 
   meta = with lib; {
