09d739b1a2d4bbd781bfb35dfebe59cfe7f392e0
rewine <1758075541@qq.com>
2022-05-17 16:34:07 +0800

    deepin-desktop-schemas: glib-compile-schemas

--- packages/go-package/deepin-desktop-schemas/default.nix
+++ packages/go-package/deepin-desktop-schemas/default.nix
@@ -4,6 +4,7 @@
 , buildGoPackage
 , go
 , go-lib
+, glib
 }:
 
 buildGoPackage rec {
@@ -19,6 +20,7 @@ buildGoPackage rec {
     sha256 = "sha256-fCI9u0eotgaay/Ci8/3bzr4MZ95GN5fH0LBXFKHJlF8=";
   };
 
+  nativeBuildInputs = [ glib ];
   buildInputs = [ go-lib ];
 
   preBuild = ''
@@ -33,6 +35,10 @@ buildGoPackage rec {
 
   installPhase = ''
     make install DESTDIR="$out" PREFIX="/" -C go/src/${goPackagePath}
+  '';
+
+  preFixup = ''
+    glib-compile-schemas ${glib.makeSchemaPath "$out" "${pname}-${version}"}
   '';
 
   meta = with lib; {
