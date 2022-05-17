6081e6207af36e51271f97919a8d21c5adb6bedd
rewine <1758075541@qq.com>
2022-05-17 16:35:14 +0800

    deepin-desktop-base: use nixos-icons

--- packages/misc/deepin-desktop-base/default.nix
+++ packages/misc/deepin-desktop-base/default.nix
@@ -1,8 +1,8 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, nixos-icons
 }:
-
 stdenv.mkDerivation rec {
   pname = "deepin-desktop-base";
   version = "2022.03.07";
@@ -14,17 +14,29 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-joAduRI9jUtPA4lNsEhgOZlci8j/cvD8rJThqvj6a8A=";
   };
 
-  makeFlags = [
-    "DESTDIR=${placeholder "out"}"
-  ];
+  propagatedBuildInputs = [ nixos-icons ];
+
+  makeFlags = [ "DESTDIR=${placeholder "out"}" ];
+
+  distribution_info = ''
+    [Distribution]
+    Name=NixOS
+    WebsiteName=www.nixos.org
+    Website=https://www.nixos.org
+    Logo=nix-snowflake.svg
+    LogoLight=nix-snowflake-white.svg
+    LogoTransparent=nix-snowflake.svg
+  '';
 
   postInstall = ''
     # Remove Deepin distro's lsb-release
     rm $out/etc/lsb-release
     # Don't override systemd timeouts
     rm -r $out/etc/systemd
-    # Remove apt-specific templates
     rm -r $out/usr/share/python-apt
+    rm -r $out/usr/share/plymouth
+    rm -r $out/usr/share/distro-info
+    echo -e ${lib.escapeShellArg distribution_info} > $out/usr/share/deepin/distribution.info
   '';
 
   meta = with lib; {
