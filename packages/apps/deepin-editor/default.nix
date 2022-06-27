7c4fa0f36c35cf02a2b1433a27023c080ad02886
rewine <1758075541@qq.com>
2022-06-27 19:52:31 +0800

    deepin-editor: 5.10.20 -> 5.10.23

--- packages/apps/deepin-editor/default.nix
+++ packages/apps/deepin-editor/default.nix
@@ -20,13 +20,13 @@
 
 stdenv.mkDerivation rec {
   pname = "deepin-editor";
-  version = "5.10.20";
+  version = "5.10.23";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-5fEcNFslihmGBPhlAZnGttxJdwc3XcbZDCabzj3CgNo=";
+    sha256 = "sha256-6K9yQhYQrbqdb5HQeszdvYPi00PMndEaaMOza5griFQ=";
   };
 
   nativeBuildInputs = [
@@ -56,13 +56,20 @@ stdenv.mkDerivation rec {
 
   patches = [
     (fetchpatch {
+      name = "do_not_include_com_deepin_dde_daemon_dock_h";
       url = "https://github.com/linuxdeepin/deepin-editor/commit/caa16d90dedf5106007d654897747f6f8f919439.patch";
       sha256 = "sha256-uUDNNvJ+JVTmbVLfdTeQ2PECbW/TbbgPWaBm0W57U60=";
-      name = "do_not_include_com_deepin_dde_daemon_dock_h";
+    })
+    (fetchpatch {
+      name = "fix_broken_KF5_include_path";
+      url = "https://github.com/linuxdeepin/deepin-editor/commit/c4b2e93f7cf0146833d2b78b516dc25f3976763f.patch";
+      sha256 = "sha256-oATccrlQBcyL6nz3ioY/9XEuHXdMqZfegFXGSowQINs=";
     })
   ];
 
   postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace "add_subdirectory(tests)" ""
     substituteInPlace src/CMakeLists.txt \
       --replace "set(CMAKE_INSTALL_PREFIX /usr)" "set(CMAKE_INSTALL_PREFIX $out)" \
       --replace "/usr/share/deepin-manual/manual-assets/application/)" "$out/share/deepin-manual/manual-assets/application/)"
