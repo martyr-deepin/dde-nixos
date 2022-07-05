5cd4768e897b2ee5442ed7b0a58668778982c254
rewine <lhongxu@outlook.com>
2022-07-05 16:40:22 +0800

    dde-dock: 5.5.9.1 -> 5.5.51

--- packages/apps/dde-dock/default.nix
+++ packages/apps/dde-dock/default.nix
@@ -21,14 +21,14 @@
 let
   rpetc = [ "/etc" "$out/etc" ];
   patchList = {
-    ## INSTALL
+    ### INSTALL
     "CMakeLists.txt" = [ ];
     "plugins/keyboard-layout/CMakeLists.txt" = [ rpetc ];
-    # RUN
+    
+    ### RUN
     "plugins/dcc-dock-plugin/settings_module.cpp" = [ ];
     "plugins/overlay-warning/overlay-warning-plugin.cpp" = [
       [ "/usr/bin/pkexec" "pkexec" ]
-      [ "/usr/sbin/overlayroot-disable" "overlayroot-disable" ] # TODO
     ];
     "plugins/overlay-warning/com.deepin.dde.dock.overlay.policy" = [
       [ "/usr/sbin/overlayroot-disable" "overlayroot-disable" ] # TODO
@@ -39,23 +39,37 @@ let
     "plugins/tray/system-trays/systemtrayscontroller.cpp" = [ ];
     "plugins/shutdown/shutdownplugin.h" = [
       rpetc
+      #"/etc/lightdm/lightdm-deepin-greeter.conf",
+      #"/etc/deepin/dde-session-ui.conf"
       [ "/usr/share/dde-session-ui/dde-session-ui.conf" "share/dde-session-ui/dde-session-ui.conf" ] # TODO
     ];
 
     "plugins/tray/indicatortray.cpp" = [ rpetc ];
     "plugins/tray/trayplugin.cpp" = [ rpetc ];
-    "frame/util/utils.h" = [ rpetc ];
+    "frame/controller/dockpluginscontroller.cpp" = [ ];
+    "frame/window/components/desktop_widget.cpp" = [ 
+      [ "/usr/lib/deepin-daemon/desktop-toggle" "${dde-daemon}/lib/deepin-daemon/desktop-toggle" ]
+     ];
+    
+    "frame/util/utils.h" = [
+     rpetc # TODO
+     # /etc/deepin/icbc.conf 
+    ];
+
+    ### OTHER
+    "dde-dock.pc.in" = [ ];
+    "cmake/DdeDock/DdeDockConfig.cmake" = [ ];
   };
 in
 stdenv.mkDerivation rec {
   pname = "dde-dock";
-  version = "5.5.9.1";
+  version = "5.5.51";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-CDL/bzTgp7/gJHcSzo8fYB858ohGEitc3AuNbQX31BY=";
+    sha256 = "sha256-eTfLdGeNa0S0TuXAI2Q8m/D73tWHKgjoBpt76+FEyaY=";
   };
 
   nativeBuildInputs = [
