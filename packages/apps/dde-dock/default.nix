1ae6f0edc7f5952c4d23e9b65d0f7e378424ba19
rewine <lhongxu@outlook.com>
2022-07-06 17:08:40 +0800

    dde-dock: fix glib schemas

--- packages/apps/dde-dock/default.nix
+++ packages/apps/dde-dock/default.nix
@@ -104,6 +104,7 @@ stdenv.mkDerivation rec {
 
   preFixup = ''
     glib-compile-schemas ${glib.makeSchemaPath "$out" "${pname}-${version}"}
+    ln -s ${glib.makeSchemaPath "$out" "${pname}-${version}"} $out/share/glib-2.0/schemas
   '';
 
   meta = with lib; {
