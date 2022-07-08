b8006ebef3ef6555b2a0ab9646ff8e8d556997e6
rewine <lhongxu@outlook.com>
2022-07-08 13:44:35 +0800

    dde-session-shell: fix path

--- packages/misc/dde-session-shell/default.nix
+++ packages/misc/dde-session-shell/default.nix
@@ -19,10 +19,113 @@
 }:
 let
   patchList = {
-    ## INSTALL
+    ### INSTALL
     "CMakeLists.txt" = [ [ "/etc" "$out/etc" ] ];
     "cmake/DdeSessionShellConfig.cmake" = [ ];
-    ## TODO patch code
+    
+    ### MISC
+    "files/com.deepin.dde.shutdownFront.service" = [
+      [ "/usr/bin/dbus-send" "dbus-send" ]
+      [ "/usr/share" "/run/current-system/sw/share"]
+      #/usr/share/applications/dde-lock.desktop
+    ];
+
+    "files/lightdm-deepin-greeter.conf" = [
+      ["/usr/share" "/run/current-system/sw/share"]
+      # /usr/share/icons/bloom
+    ];
+
+    "files/wayland/lightdm-deepin-greeter-wayland.desktop" = [
+      [ "/usr/bin/deepin-greeter" "deepin-greeter" ]
+    ];
+    "files/lightdm-deepin-greeter.desktop" = [
+      [ "/usr/bin/deepin-greeter" "deepin-greeter" ]
+    ];
+    "files/com.deepin.dde.lockFront.service" = [
+      [ "/usr/bin/dbus-send" "dbus-send" ]
+      [ "/usr/share" "/run/current-system/sw/share" ]
+      #/usr/share/applications/dde-lock.desktop
+    ];
+    "files/dde-lock.desktop" = [
+      [ "/usr/bin/dde-lock" "dde-lock" ]
+    ];
+
+    ### CODE
+    "scripts/lightdm-deepin-greeter" = [
+      # TODO ["/usr/lib/deepin-daemon/greeter-display-daemon"]
+      ["/usr/bin/lightdm-deepin-greeter" "lightdm-deepin-greeter"]
+    ];
+
+    "src/widgets/shutdownwidget.cpp" = [
+      ["/usr/bin/deepin-system-monitor" "deepin-system-monitor" ]
+    ];
+
+    "src/lightdm-deepin-greeter/greeterworker.cpp" = [
+      ["/usr/sbin/lightdm" "lightdm"]
+      # /etc/deepin/no_suspend
+    ];
+    "src/session-widgets/userinfo.cpp" = [
+      # TODO
+      # ["/usr/share/backgrounds/default_background.jpg"]
+    ];
+    "src/widgets/fullscreenbackground.cpp" = [
+      # TODO
+      #"/usr/share/backgrounds/default_background.jpg"
+    ];
+    "src/session-widgets/lockcontent.cpp" = [
+      # TODO
+      # "/usr/share/backgrounds/deepin/desktop.jpg"
+    ];
+    "src/session-widgets/auth_module.h" = [
+      # TODO
+      # "/usr/lib/dde-control-center/reset-password-dialog"
+    ];
+    "files/wayland/kwin_wayland_helper-wayland" = [
+      [ "/usr/bin/kwin_wayland" "kwin_wayland" ]
+    ];
+    "files/wayland/deepin-greeter-wayland" = [
+      # export QML2_IMPORT_PATH=/usr/lib/x86_64-linux-gnu/qt5/qml
+      # export XDG_DATA_DIRS=/usr/share
+      [ "/usr/bin/kwin_wayland" "kwin_wayland" ]
+      # "/etc/xdg"
+      # "/etc/deepin/greeters.d/lightdm-deepin-greeter"
+    ];
+    "src/global_util/public_func.cpp" = [
+      # TODO
+      # /usr/share/dde-session-shell/translations
+    ];
+    "src/global_util/modules_loader.cpp" = [
+      # TODO
+      # /usr/lib/dde-session-shell/modules
+    ];
+    "src/global_util/xkbparser.h" = [
+      #? /usr/share/X11/xkb/rules/base.xml
+    ];
+    "src/global_util/constants.h" = [
+      [ "/usr/share/icons" "/run/current-system/sw/share/icons" ]
+      #"/usr/share/icons/default/index.theme"
+      
+      #"/usr/share/dde-session-shell/dde-session-shell.conf",
+      
+      #"/usr/share/dde-session-ui/dde-session-ui.conf"
+      #"/usr/share/dde-session-ui/dde-shutdown.conf
+    ];
+    "files/wayland/lightdm-deepin-greeter-wayland" = [
+      #? /usr/lib/deepin-daemon/greeter-display-daemon
+      [ "/usr/bin/lightdm-deepin-greeter" "lightdm-deepin-greeter" ]
+      # TODO export QT_QPA_PLATFORM_PLUGIN_PATH...
+    ];
+
+    # scripts/lightdm-deepin-greeter /etc/lightdm/deepin/xsettingsd.conf
+    # files/deepin-greeter /etc/deepin/greeters.d/
+    # files/wayland/kwin_wayland_helper-wayland /etc/xdg/kglobalshortcutsrc
+
+    "src/app/lightdm-deepin-greeter.cpp" = [
+      # /etc/lightdm/deepin/xsettingsd.conf
+    ];
+    "src/lightdm-deepin-greeter/deepin-greeter" = [
+      # /etc/deepin/greeters.d/
+    ];
   };
 in
 stdenv.mkDerivation rec {
