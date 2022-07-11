739cc8f64502733fb6196072d710019d27e70bf3
rewine <lhongxu@outlook.com>
2022-07-11 17:02:33 +0800

    vm: add dconf-editor

--- vm/flake.nix
+++ vm/flake.nix
@@ -19,6 +19,7 @@
           firefox
           neovim
           gdb
+          gnome.dconf-editor
         ];
         services.xserver = {
           enable = true;
@@ -38,7 +39,7 @@
         };
         time.timeZone = "Asia/Shanghai";
         i18n = {
-          defaultLocale = "zh_CN.UTF-8";
+          defaultLocale = "en_US.UTF-8/UTF-8";
           supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
           inputMethod.enabled = "fcitx";
         };
