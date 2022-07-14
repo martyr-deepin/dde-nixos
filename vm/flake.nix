89cb806c1c6e9c502858db326fc79992529afd38
rewine <lhongxu@outlook.com>
2022-07-14 14:41:09 +0800

    vm: update config

--- vm/flake.nix
+++ vm/flake.nix
@@ -47,7 +47,7 @@
         };
         time.timeZone = "Asia/Shanghai";
         i18n = {
-          defaultLocale = "en_US.UTF-8/UTF-8";
+          defaultLocale = "zh_CN.UTF-8/UTF-8";
           supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
           inputMethod.enabled = "fcitx";
         };
