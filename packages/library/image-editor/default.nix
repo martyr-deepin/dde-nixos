dbf31f9124cfebdf071db7af70f605b8de47ea7a
rewine <lhongxu@outlook.com>
2022-07-18 14:34:02 +0800

    image-editor: 1.9.10 -> 1.0.17

--- packages/library/image-editor/default.nix
+++ packages/library/image-editor/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, fetchpatch
 , dtk
 , cmake
 , qttools
@@ -8,17 +9,18 @@
 , wrapQtAppsHook
 , opencv
 , freeimage
+, libmediainfo
 }:
 
 stdenv.mkDerivation rec {
   pname = "image-editor";
-  version = "1.9.10";
+  version = "1.0.17";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-6NRjSkOOGIYghSoibf8moJKRMiepytHk+pn+zjD04mU=";
+    sha256 = "sha256-Oekryl18o1wzXI2OiRLvpcZfPMvoLqODCO1VU0mB8bA=";
   };
 
   nativeBuildInputs = [ cmake pkgconfig qttools wrapQtAppsHook ];
@@ -27,19 +29,23 @@ stdenv.mkDerivation rec {
     dtk
     opencv
     freeimage
+    libmediainfo
   ];
 
-  postPatch = ''
-    substituteInPlace libimageviewer/CMakeLists.txt \
-      --replace "set(PREFIX /usr)" "set(PREFIX $out)"
-    substituteInPlace libimagevisualresult/CMakeLists.txt \
-      --replace "set(PREFIX /usr)" "set(PREFIX $out)"
-  '';
+  cmakeFlags = [ "-DCMAKE_INSTALL_LIBDIR=lib" ];
 
-  # fix bug https://github.com/linuxdeepin/developer-center/issues/2234
-  postInstall = ''
-    cp -r  $out/include/libimageviewer/* $out/include/
-  '';
+  patches = [
+    (fetchpatch {
+      name = "fix_use_correct_LIBDIR_for_pkgconfig";
+      url = "https://github.com/linuxdeepin/image-editor/commit/8b2066ee16995442e1c4891c75f26d80bbb7a483.patch";
+      sha256 = "sha256-548t4UjSx1AcKG1nBm6BSIlPASwyDZQ+Rp9H+dr7sOc=";
+    })
+    (fetchpatch {
+      name = "feat_check_PREFIX_value_before_set";
+      url = "https://github.com/linuxdeepin/image-editor/commit/a7c6655c57184952c10387771737f92b950246a5.patch";
+      sha256 = "sha256-uGRKCkzZGFcwT0n9JFK3SuMWilaXDyqyAYh+VUdTo/w=";
+    })
+  ];
 
   meta = with lib; {
     description = "image editor lib for dtk";
