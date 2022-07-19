3a8e78d08dc60aa69ffb8c8cd9839a15861457d7
rewine <lhongxu@outlook.com>
2022-07-19 10:38:16 +0800

    gio-qt: fix pkgconfig

--- packages/library/gio-qt/default.nix
+++ packages/library/gio-qt/default.nix
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake pkgconfig wrapQtAppsHook ];
 
-  cmakeFlags = lib.optional (!buildDocs) [ "-DBUILD_DOCS=OFF" ];
+  cmakeFlags = [ "-DCMAKE_INSTALL_LIBDIR=lib" ] ++ lib.optional (!buildDocs) [ "-DBUILD_DOCS=OFF" ];
 
   buildInputs = lib.optional buildDocs doxygen;
 
