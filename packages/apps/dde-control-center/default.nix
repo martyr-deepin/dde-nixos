2dc5804e7607373db24faac7fa2f1bcf35d9c728
rewine <lhongxu@outlook.com>
2022-07-21 11:00:52 +0800

    dde-control-center: fix path

--- packages/apps/dde-control-center/default.nix
+++ packages/apps/dde-control-center/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, getPatchFrom
 , dtk
 , substituteAll
 , qt5integration
@@ -30,8 +31,70 @@
 , networkmanager-qt
 , glib
 , gtest
+, runtimeShell
+, tzdata
 }:
+let
+  patchList = {
+    "com.deepin.controlcenter.develop.policy" = [
+      # "/usr/lib/dde-control-center/develop-tool"
+    ];
+    "dde-control-center-autostart.desktop" = [
+      # /usr/bin/dde-control-center
+    ];
+    "abrecovery/deepin-ab-recovery.desktop" = [
+      # /usr/bin/abrecovery
+    ];
+    "com.deepin.dde.ControlCenter.service" = [
+      [ "/usr/bin/dbus-send" "dbus-send" ]
+      # "/usr/share/applications/dde-control-center.desktop
+    ];
+    "dde-control-center-wapper" = [
+      # /usr/share/applications/dde-control-center.desktop
+      #? qdbus
+    ];
 
+    "src/reboot-reminder-dialog/main.cpp" = [
+      # /usr/share/dde-control-center/translations/dialogs_
+    ];
+    "src/frame/main.cpp" = [
+      # /usr/share/dde-control-center/translations/keyboard_language_
+    ];
+    "src/frame/window/insertplugin.cpp" = [
+      # /usr/lib/dde-control-center/modules
+    ];
+    "src/frame/window/mainwindow.cpp" = [
+      # /usr/lib/dde-control-center/modules
+      #? /usr/share/icons/bloom/apps/64/preferences-system.svg
+    ];
+    "src/frame/window/protocolfile.cpp" = [
+      #? /usr/share/protocol
+      #? /usr/share/deepin-deepinid-client/
+    ];
+    "include/widgets/utils.h" = [
+      # "/usr/share/dde-control-center/dde-control-center.conf" 
+    ];
+    "src/reset-password-dialog/main.cpp" = [
+      # /usr/share/dde-control-center/translations/reset-password-dialog_
+    ];
+    "src/frame/modules/update/updatework.cpp" = [
+      #? /usr/share/deepin/
+    ];
+    "src/frame/modules/datetime/timezone_dialog/timezone.cpp" = [
+      [ "usr/share/zoneinfo" "${tzdata}/share/zoneinfo" ]
+      # /usr/share/zoneinfo/zone1970.tab
+    ];
+    "src/frame/modules/keyboard/customedit.cpp" = [
+      #? /usr/bin
+    ];
+    "abrecovery/main.cpp" = [
+      # /usr/share/dde-control-center/translations/recovery_
+    ];
+    "src/frame/modules/accounts/accountsworker.cpp" = [
+      [ "/bin/bash" "${runtimeShell}" ]
+    ];
+  };
+in
 stdenv.mkDerivation rec {
   pname = "dde-control-center";
   version = "unstable-2022-07-06";
@@ -42,6 +105,34 @@ stdenv.mkDerivation rec {
     rev = "5214b2756a13a8a27fe414280d1ab8e3d990454f";
     sha256 = "sha256-aB2vH8Uz06zqBsW9KBlzr8Z7S5HxDS7bZ2qxg95lSuY=";
   };
+
+  patches = [
+    (substituteAll {
+      src = ./0001-fix-path.patch;
+      qtbase_dev = "${lib.getDev qtbase}/include/QtXkbCommonSupport/5.15.3";
+    })
+  ];
+
+  fixInstallPatch = ''
+    substituteInPlace src/frame/CMakeLists.txt \
+      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)'
+
+    substituteInPlace src/develop-tool/CMakeLists.txt \
+      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)'
+
+    substituteInPlace abrecovery/CMakeLists.txt \
+      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)' \
+      --replace '/usr/bin)' 'bin)' \
+      --replace '/etc/xdg/autostart)' 'etc/xdg/autostart)'
+
+    substituteInPlace src/reboot-reminder-dialog/CMakeLists.txt \
+      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)'
+
+    substituteInPlace src/reset-password-dialog/CMakeLists.txt \
+      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)'
+  '';
+
+  postPatch = fixInstallPatch + getPatchFrom patchList;
 
   nativeBuildInputs = [
     cmake
@@ -76,46 +167,6 @@ stdenv.mkDerivation rec {
     gtest
   ];
 
-  qtWrapperArgs = [
-    "--prefix QT_PLUGIN_PATH : ${qt5integration}/plugins"
-    "--prefix QT_QPA_PLATFORM_PLUGIN_PATH : ${qt5platform-plugins}/plugins"
-  ];
-
-  patches = [
-    (substituteAll {
-      src = ./0001-fix-path.patch;
-      qtbase_dev = "${lib.getDev qtbase}/include/QtXkbCommonSupport/5.15.3";
-    })
-  ];
-
-  ### TODO include/widgets/utils.h abrecovery/main.cpp dde-control-center-autostart.desktop com.deepin.dde.ControlCenter.service src/frame/window/protocolfile.cpp ...
-
-  fixInstallPatch = ''
-    substituteInPlace src/frame/CMakeLists.txt \
-      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)'
-
-    substituteInPlace src/develop-tool/CMakeLists.txt \
-      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)'
-
-    substituteInPlace abrecovery/CMakeLists.txt \
-      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)' \
-      --replace '/usr/bin)' 'bin)' \
-      --replace '/etc/xdg/autostart)' 'etc/xdg/autostart)'
-
-    substituteInPlace src/reboot-reminder-dialog/CMakeLists.txt \
-      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)'
-
-    substituteInPlace src/reset-password-dialog/CMakeLists.txt \
-      --replace 'set(CMAKE_INSTALL_PREFIX /usr)' 'set(CMAKE_INSTALL_PREFIX $out)'
-  '';
-
-  fixPolicyPatch = ''
-    substituteInPlace com.deepin.controlcenter.develop.policy \
-      --replace '/usr/lib/dde-control-center/develop-tool' 'out/lib/dde-control-center/develop-tool'
-  '';
-
-  postPatch = fixInstallPatch + fixPolicyPatch;
-
   cmakeFlags = [
     "-DVERSION=${version}"
     "-DDISABLE_AUTHENTICATION=YES"
@@ -125,6 +176,11 @@ stdenv.mkDerivation rec {
     "-DCMAKE_INSTALL_LIBDIR=lib"
     "-DINCLUDE_INSTALL_DIR=include"
     #"-DDCMAKE_INSTALL_COMPONENT=false"
+  ];
+
+  qtWrapperArgs = [
+    "--prefix QT_PLUGIN_PATH : ${qt5integration}/plugins"
+    "--prefix QT_QPA_PLATFORM_PLUGIN_PATH : ${qt5platform-plugins}/plugins"
   ];
 
   preFixup = ''
