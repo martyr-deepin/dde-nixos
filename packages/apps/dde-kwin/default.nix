b2aa6e52a5d7c5fad671ff91a79345344d3598d9
rewine <lhongxu@outlook.com>
2022-07-26 11:46:50 +0800

    dde-kwin: add patch

--- packages/apps/dde-kwin/default.nix
+++ packages/apps/dde-kwin/default.nix
@@ -1,15 +1,29 @@
 { stdenv
 , lib
 , getPatchFrom
+, getShebangsPatchFrom
 , pkg-config
 , fetchFromGitHub
 , cmake
 , kwin
 , kwayland
 , qttools
+, extra-cmake-modules
+, dtk
+, gsettings-qt
+, xorg
+, libepoxy
 }:
 let
   patchList = {
+    ### BUILD
+    "translate_desktop2ts.sh" = [
+      [ "/usr/bin/deepin-desktop-ts-convert" "deepin-desktop-ts-convert" ]
+    ];
+    "translate_ts2desktop.sh" = [
+      [ "/usr/bin/deepin-desktop-ts-convert" "deepin-desktop-ts-convert" ]
+    ];
+    ### INSTALL
     "CMakeLists.txt" = [
       # TODO
       [ "/usr/include/KWaylandServer" "${kwayland.dev}/include/KWaylandServer" ]
@@ -17,6 +31,9 @@ let
     ];
     "configures/CMakeLists.txt" = [
       [ "/etc/xdg" "$out/etc/xdg" ]
+    ];
+    "configures/kwin_no_scale.in" = [
+      [ "kwin 5.21.5" "kwin ${kwin.version}" ] # TODO
     ];
   };
 in
@@ -31,30 +48,49 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-j/dgGt5zNaVHZF+DrEK8aRdYanPxcvePwqKmc7KM8gk=";
   };
 
+  patches = [
+    ./dde-kwin.5.4.26.patch
+    ./deepin-kwin-tabbox-chameleon-rename.patch
+  ];
+
+  postPatch = ''
+    patch -Rp1 -i ${./deepin-kwin-added-functions-from-their-forked-kwin.patch}
+  '' + getPatchFrom patchList + getShebangsPatchFrom [
+    "configures/kwin_no_scale.in"
+    "translate_desktop2ts.sh"
+    "translate_ts2desktop.sh"
+    "plugins/platforms/plugin/translate_generation.sh"
+  ];
+
   nativeBuildInputs = [
     cmake
     qttools
     #deepin-gettext-tools
-    #extra-cmake-modules
+    extra-cmake-modules
     pkg-config
   ];
 
   buildInputs = [
     kwin
     kwayland
+    dtk
+    gsettings-qt
+    xorg.libXdmcp
+    libepoxy.dev
   ];
 
   dontWrapQtApps = true;
 
-  #NIX_CFLAGS_COMPILE = [
-  #  "-I${kwayland.dev}/include/KF5"
-  #];
-
-  cmakeFlags = [
-    "-DKWIN_VERSION=${kwin.version}"
+  NIX_CFLAGS_COMPILE = [
+   "-I${kwayland.dev}/include/KF5"
   ];
 
-  postPatch = getPatchFrom patchList;
+  cmakeFlags = [
+    "-DPROJECT_VERSION=${version}"
+    "-DKWIN_VERSION=${kwin.version}"
+    "-DUSE_DEEPIN_WAYLAND=OFF"
+    "-DUSE_WINDOW_TOOL=OFF"
+  ];
 
   meta = with lib; {
     description = "KWin configuration for Deepin Desktop Environment";
