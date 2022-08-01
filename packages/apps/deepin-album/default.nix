ff744f88bbb5529536867e0513d078a63415b676
rewine <lhongxu@outlook.com>
2022-08-01 15:48:12 +0800

    deepin-album: 5.10.2 -> 5.10.4

--- packages/apps/deepin-album/default.nix
+++ packages/apps/deepin-album/default.nix
@@ -21,14 +21,32 @@
 
 stdenv.mkDerivation rec {
   pname = "deepin-album";
-  version = "5.10.2";
+  version = "5.10.4";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-F3s+smjKwY2ypGVsuA5OwR+a+ZnC7f4C7zpT3bTxtpw=";
+    sha256 = "sha256-dexYchGo3znD/4q+Ja7e6mhckxwAgEP3ZDuemtPwiSo=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "use_pkgconfig_to_find_libimageviewer";
+      url = "https://github.com/linuxdeepin/deepin-album/commit/8a8ba283129e336b9598531505840ebf7f915e17.patch";
+      sha256 = "sha256-YY1VahcXRgmbuUts6Oa5agyKznnf7bnaADA6iWj5rn4=";
+    })
+  ];
+
+  postPatch = ''
+    substituteInPlace libUnionImage/CMakeLists.txt \
+      --replace "/usr" "$out" \
+    
+    substituteInPlace src/CMakeLists.txt \
+      --replace "set(PREFIX /usr)" "set(PREFIX $out)" \
+      --replace "/usr/bin" "$out/bin" \
+      --replace "/usr/share/deepin-manual/manual-assets/application/)" "share/deepin-manual/manual-assets/application/)"
+  '';
 
   nativeBuildInputs = [ cmake pkgconfig qttools wrapQtAppsHook ];
 
@@ -50,24 +68,6 @@ stdenv.mkDerivation rec {
   ];
 
   cmakeFlags = [ "-DVERSION=${version}" ];
-
-  patches = [
-    (fetchpatch {
-      name = "use_pkgconfig_to_find_libimageviewer";
-      url = "https://github.com/linuxdeepin/deepin-album/commit/8a8ba283129e336b9598531505840ebf7f915e17.patch";
-      sha256 = "sha256-YY1VahcXRgmbuUts6Oa5agyKznnf7bnaADA6iWj5rn4=";
-    })
-  ];
-
-  postPatch = ''
-    substituteInPlace libUnionImage/CMakeLists.txt \
-      --replace "set(PREFIX /usr)" "set(PREFIX $out)" \
-    
-    substituteInPlace src/CMakeLists.txt \
-      --replace "set(PREFIX /usr)" "set(PREFIX $out)" \
-      --replace "/usr/bin" "$out/bin" \
-      --replace "/usr/share/deepin-manual/manual-assets/application/)" "share/deepin-manual/manual-assets/application/)"
-  '';
 
   meta = with lib; {
     description = "A fashion photo manager for viewing and organizing pictures";
