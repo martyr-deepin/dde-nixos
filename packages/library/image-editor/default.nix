9dfc6588ec3703fdabd3c2f5c97f3d005bde4683
rewine <lhongxu@outlook.com>
2022-08-01 15:47:11 +0800

    image-editor: 1.0.17 -> 1.0.18+

--- packages/library/image-editor/default.nix
+++ packages/library/image-editor/default.nix
@@ -10,18 +10,32 @@
 , opencv
 , freeimage
 , libmediainfo
+, pcre
 }:
 
 stdenv.mkDerivation rec {
   pname = "image-editor";
-  version = "1.0.17";
+  version = "1.0.18+";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-Oekryl18o1wzXI2OiRLvpcZfPMvoLqODCO1VU0mB8bA=";
+    rev = "c06fb00a6ac8a6c27a062fa2e7a5756c69e707b7";
+    sha256 = "sha256-/YGlZ0GWYQLmLRqy3lUNkG7z1vkmj6ywj8BJJga1Hqs=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "feat_check_PREFIX_value_before_set";
+      url = "https://github.com/linuxdeepin/image-editor/commit/a7c6655c57184952c10387771737f92b950246a5.patch";
+      sha256 = "sha256-uGRKCkzZGFcwT0n9JFK3SuMWilaXDyqyAYh+VUdTo/w=";
+    })
+  ];
+
+  postPatch = ''
+    substituteInPlace libimageviewer/CMakeLists.txt \
+      --replace "/usr" "$out"
+  '';
 
   nativeBuildInputs = [ cmake pkgconfig qttools wrapQtAppsHook ];
 
@@ -30,22 +44,10 @@ stdenv.mkDerivation rec {
     opencv
     freeimage
     libmediainfo
+    pcre
   ];
 
   cmakeFlags = [ "-DCMAKE_INSTALL_LIBDIR=lib" ];
-
-  patches = [
-    (fetchpatch {
-      name = "fix_use_correct_LIBDIR_for_pkgconfig";
-      url = "https://github.com/linuxdeepin/image-editor/commit/8b2066ee16995442e1c4891c75f26d80bbb7a483.patch";
-      sha256 = "sha256-548t4UjSx1AcKG1nBm6BSIlPASwyDZQ+Rp9H+dr7sOc=";
-    })
-    (fetchpatch {
-      name = "feat_check_PREFIX_value_before_set";
-      url = "https://github.com/linuxdeepin/image-editor/commit/a7c6655c57184952c10387771737f92b950246a5.patch";
-      sha256 = "sha256-uGRKCkzZGFcwT0n9JFK3SuMWilaXDyqyAYh+VUdTo/w=";
-    })
-  ];
 
   meta = with lib; {
     description = "image editor lib for dtk";
