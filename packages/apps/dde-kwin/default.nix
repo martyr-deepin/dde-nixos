1df8b56b70f4a1d29b46071983a37595e59c8556
rewine <lhongxu@outlook.com>
2022-08-04 15:59:04 +0800

    dde-kwin: fix build

--- packages/apps/dde-kwin/default.nix
+++ packages/apps/dde-kwin/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, stdenvNoCC
 , lib
 , getPatchFrom
 , getShebangsPatchFrom
@@ -8,6 +9,7 @@
 , kwin
 , kwayland
 , qttools
+, wrapQtAppsHook
 , deepin-gettext-tools
 , extra-cmake-modules
 , dtk
@@ -34,8 +36,38 @@ let
       [ "/etc/xdg" "$out/etc/xdg" ]
     ];
     "configures/kwin_no_scale.in" = [
+      [ "/etc/xdg/kglobalshortcutsrc" "$out/etc/xdg/kglobalshortcutsrc" ]
       [ "kwin 5.21.5" "kwin ${kwin.version}" ] # TODO
     ];
+    "plugins/platforms/lib/dde-kwin.pc.in" = [
+      [ "/usr/X11R6/lib64" "$out/lib" ] # FIXME:
+    ];
+
+    ### 
+    "plugins/kwineffects/multitasking/background.cpp" = [
+      # TODO: file:///usr/share/wallpapers/deepin/desktop.jpg
+    ];
+    "deepin-wm-dbus/deepinwmfaker.cpp" = [
+      # TODO: file:///usr/share/wallpapers/deepin/desktop.jpg
+      # /usr/lib/deepin-daemon/dde-warning-dialog
+    ];
+    "plugins/platforms/plugin/main_wayland.cpp" = [ ];
+    "plugins/platforms/plugin/main.cpp" = [ ];
+
+  };
+
+  libkwin = stdenvNoCC.mkDerivation rec {
+    pname = "libkwin";
+    version = kwin.version;
+    src = kwin.out;
+    propagatedBuildInputs = [ kwin ];
+    dotBuild = true;
+    dontWrapQtApps = true;
+    installPhase = ''
+      mkdir -p $out/lib
+      ln -sf $src/lib/libkwin.so.${version} $out/lib/libkwin.so
+    '';
+    dontFixup = true;
   };
 in
 stdenv.mkDerivation rec {
@@ -50,12 +82,17 @@ stdenv.mkDerivation rec {
   };
 
   patches = [
+    ./0001-feat-check-PLUGIN_INSTALL_PATH-value-before-set.patch
     ./dde-kwin.5.4.26.patch
     ./deepin-kwin-tabbox-chameleon-rename.patch
   ];
 
   postPatch = ''
     patch -Rp1 -i ${./deepin-kwin-added-functions-from-their-forked-kwin.patch}
+
+    sed -i '/add_subdirectory(kdecoration)/d' plugins/CMakeLists.txt || die
+    sed -i 's|GLRenderTarget|GLFramebuffer|g' plugins/kwineffects/scissor-window/scissorwindow.cpp || die
+    sed -i '/(!w->isPaintingEnabled() || (mask & PAINT_WINDOW_LANCZOS)/,+2d' plugins/kwineffects/scissor-window/scissorwindow.cpp || die
   '' + getPatchFrom patchList + getShebangsPatchFrom [
     "configures/kwin_no_scale.in"
     "translate_desktop2ts.sh"
@@ -69,18 +106,18 @@ stdenv.mkDerivation rec {
     deepin-gettext-tools
     extra-cmake-modules
     pkg-config
+    wrapQtAppsHook
   ];
 
   buildInputs = [
     kwin
+    libkwin
     kwayland
     dtk
     gsettings-qt
     xorg.libXdmcp
     libepoxy.dev
   ];
-
-  dontWrapQtApps = true;
 
   NIX_CFLAGS_COMPILE = [
     "-I${kwayland.dev}/include/KF5"
@@ -95,8 +132,10 @@ stdenv.mkDerivation rec {
     "-DENABLE_BUILTIN_MULTITASKING=OFF"
     "-DENABLE_BUILTIN_BLACK_SCREEN=OFF"
     "-DUSE_DEEPIN_WAYLAND=OFF"
+    "-DPLUGIN_INSTALL_PATH=${placeholder "out"}/lib/platforms"
 
     "-DENABLE_BUILTIN_SCISSOR_WINDOW=OFF" # TODO
+    "-DKWIN_LIBRARY_PATH=${libkwin}/lib"
   ];
 
   meta = with lib; {
