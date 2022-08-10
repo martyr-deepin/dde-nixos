fd28634c7cb8d6c4a2d9c3a7aff15c4bee59cac2
rewine <lhongxu@outlook.com>
2022-08-10 16:24:05 +0800

    dde-session-ui: fix build

--- packages/misc/dde-session-ui/default.nix
+++ packages/misc/dde-session-ui/default.nix
@@ -110,7 +110,7 @@ stdenv.mkDerivation rec {
     gtest
   ];
 
-  NIX_CFLAGS_COMPILE = "-I${dde-dock}/include/dde-dock";
+  NIX_CFLAGS_COMPILE = "-I${dde-dock.dev}/include/dde-dock";
 
   preFixup = ''
     glib-compile-schemas ${glib.makeSchemaPath "$out" "${pname}-${version}"}
