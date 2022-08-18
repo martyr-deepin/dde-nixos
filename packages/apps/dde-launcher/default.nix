29aa5f895f0e0eab7d47844f9ca7bd7cd561c078
rewine <luhongxu@uniontech.com>
2022-08-18 15:00:39 +0800

    deepin-launcher: update patch

--- packages/apps/dde-launcher/default.nix
+++ packages/apps/dde-launcher/default.nix
@@ -2,6 +2,7 @@
 , lib
 , fetchFromGitHub
 , getPatchFrom
+, fetchpatch
 , dtk
 , dde-qt-dbus-factory
 , qt5integration
@@ -21,9 +22,6 @@
 }:
 let
   patchList = {
-    ### INSTALL
-    "CMakeLists.txt" = [ ];
-
     ### MISC
     "dde-launcher.desktop" = [
       # "/usr/bin/dde-launcher"
@@ -48,14 +46,22 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "dde-launcher";
-  version = "5.5.19";
+  version = "5.5.30";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-8GusAwDGTfqoqHr+oV6S3OzMXUgqkyzOGnkczx5B6Us=";
+    sha256 = "sha256-C6VPlfn89y5YLnkumG4iBOg9mf+GQ368B0BLp4MPPRo=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "chore: use GNUInstallDirs in CmakeLists";
+      url = "https://github.com/linuxdeepin/dde-launcher/commit/121d6048092c7afc96d788e360445644e4fb95dd.diff";
+      sha256 = "sha256-14hhnBWWURYuy0rbO/y2mRp6ktm6cxcZOPrVI/ruAKc=";
+    })
+  ];
 
   postPatch = getPatchFrom patchList;
 
