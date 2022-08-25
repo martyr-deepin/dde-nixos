9dc1bfedc1e675354194d3b9a8d3b3fd69c6f5e2
rewine <luhongxu@uniontech.com>
2022-08-25 13:59:17 +0800

    deepin-movie-reborn: 5.9.14 -> 5.10.8

--- packages/apps/deepin-movie-reborn/default.nix
+++ packages/apps/deepin-movie-reborn/default.nix
@@ -12,6 +12,7 @@
 , pkgconfig
 , qttools
 , qtx11extras
+, qtmultimedia
 , wrapQtAppsHook
 , gsettings-qt
 , elfutils
@@ -43,24 +44,54 @@ let
       --replace 'libPath("libavformat.so")'           '"${ffmpeg.out}/lib/libavformat.so"' \
       --replace 'libPath("libavutil.so")'             '"${ffmpeg.out}/lib/libavutil.so"' \
       --replace 'libPath("libffmpegthumbnailer.so")'  '"${ffmpegthumbnailer.out}/lib/libffmpegthumbnailer.so"' \
-      --replace 'libPath("libmpv.so.1")'              '"${mpv}/lib/libmpv.so.1"'
+      --replace 'libPath("libmpv.so.1")'              '"${mpv}/lib/libmpv.so.1"' \
+      --replace 'libPath("libgstreamer-1.0.so")'      '"${gst_all_1.gstreamer.out}/lib/libgstreamer-1.0.so"' \
+      --replace 'libPath("libgstpbutils-1.0.so")'     '"${gst_all_1.gst-plugins-base.out}/lib/libgstpbutils-1.0.so"'
+
   '' + lib.optionalString cudaSupport '' 
+  
   '';
   ### TODO src/backends/mpv/mpv_proxy.cpp libgpuinfo.so
 
 in
 stdenv.mkDerivation rec {
   pname = "deepin-movie-reborn";
-  # version = "5.10.2";
-  version = "5.9.14";
+  version = "5.10.8";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    # sha256 = "sha256-247dOKfNq8A3Ngxshy6q9mlgy32kpmpAOS/I2u0Vgzo=";
-    sha256 = "sha256-crTAXTuStA3euPvq/h97wzuG+wTz83biqXgvxkERLRg=";
+    sha256 = "sha256-tBUeYbsJDwuzcOt89nMeP+taZPz4lb27qEQjCxZsMck=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "chore: use GNUInstallDirs in CmakeLists";
+      url = "https://github.com/linuxdeepin/deepin-movie-reborn/commit/15627ec622f8da9445fd25d2775a9b0f67618f07.patch";
+      sha256 = "sha256-p3kJE7lSeqasLSswVOj91CFRZk8kRekT13ZAGKenwvU=";
+    })
+    (fetchpatch {
+      name = "chore: dont use </usr/include/linux/cdrom.h>";
+      url = "https://github.com/linuxdeepin/deepin-movie-reborn/commit/2afc63541589adab8b0c8c48e290f03535ec2996.patch";
+      sha256 = "sha256-Q9dv5L5sUGeuvNxF8ypQlZuZVuU4NIR/8d8EyP/Q5wk=";
+    })
+  ];
+
+  fixLoadLibPatch = lib.concatMapStrings replaceLibPath [
+    "src/backends/mpv/mpv_proxy.cpp"
+    "src/common/hwdec_probe.cpp"
+    "src/common/thumbnail_worker.cpp"
+    "src/common/platform/platform_thumbnail_worker.cpp"
+    "src/libdmr/gstutils.cpp"
+    "src/libdmr/filefilter.cpp"
+    "src/libdmr/playlist_model.cpp"
+    "src/widgets/toolbox_proxy.cpp"
+    "src/widgets/platform/platform_toolbox_proxy.cpp"
+    "src/backends/mpv/mpv_glwidget.cpp"
+  ];
+
+  postPatch = fixLoadLibPatch;
 
   outputs = [ "out" "dev" ];
 
@@ -74,6 +105,7 @@ stdenv.mkDerivation rec {
   buildInputs = [
     dtk
     qtx11extras
+    qtmultimedia
     qtdbusextended
     qtmpris
     gsettings-qt
@@ -96,70 +128,27 @@ stdenv.mkDerivation rec {
     gst_all_1.gstreamer
   ] ++ lib.optional cudaSupport [ cudaPackages.cudatoolkit ];
 
-  patches = [
-    ./fix-build.patch
-  ];
-
   qtWrapperArgs = [ 
     "--prefix QT_PLUGIN_PATH : ${qt5integration}/${qtbase.qtPluginPrefix}"
     "--prefix XDG_DATA_DIRS : ${placeholder "out"}/share/gsettings-schemas/${pname}-${version}"
   ];
 
+  NIX_CFLAGS_COMPILE = [
+    "-I${gst_all_1.gstreamer.dev}/include/gstreamer-1.0"
+    "-I${gst_all_1.gst-plugins-base.dev}/include/gstreamer-1.0"
+  ];
 
   cmakeFlags = [
     "-DVERSION=${version}"
+    "-D_LIBMPR_=NO"
   ];
-
-  fixCodePatch = ''
-    substituteInPlace src/common/diskcheckthread.cpp \
-      --replace "</usr/include/linux/cdrom.h>" "<linux/cdrom.h>"
-
-    substituteInPlace tests/deepin-movie-platform/CMakeLists.txt \
-      --replace "find_package(gui-private)" ""
-    substituteInPlace tests/deepin-movie/CMakeLists.txt \
-      --replace "find_package(gui-private)" ""
-    substituteInPlace src/CMakeLists.txt \
-      --replace "find_package(gui-private)" ""
-  '';
-
-  fixInstallPatch = ''
-    substituteInPlace src/CMakeLists.txt \
-      --replace "/usr/share/glib-2.0/schemas)" "$out/share/glib-2.0/schemas)" \
-      --replace "/usr/share/deepin-manual/manual-assets/application/)" "$out/share/deepin-manual/manual-assets/application/)"
-  '';
-
-  fixLoadLibPatch = lib.concatMapStrings replaceLibPath [
-    "src/libdmr/playlist_model.cpp"
-    "src/libdmr/filefilter.cpp"
-    "src/widgets/platform/platform_toolbox_proxy.cpp"
-    "src/common/platform/platform_thumbnail_worker.cpp"
-    "src/common/thumbnail_worker.cpp"
-    "src/common/hwdec_probe.cpp"
-    "src/widgets/toolbox_proxy.cpp"
-    "src/backends/mpv/mpv_proxy.cpp"
-    "src/backends/mpv/mpv_glwidget.cpp"
-    "src/backends/mpv/mpv_glwidget.cpp"
-  ];
-
-  fixDevFilesPatch = ''
-    substituteInPlace src/libdmr/libdmr.pc.in \
-      --replace "prefix=/usr" "prefix=$dev" \
-      --replace "@CMAKE_INSTALL_LIBDIR@" "lib" \
-      --replace "@PROJECT_VERSION@" "${version}"
-  '';
-
-  postPatch = fixCodePatch + fixInstallPatch + fixLoadLibPatch + fixDevFilesPatch;
 
   preFixup = ''
     glib-compile-schemas ${glib.makeSchemaPath "$out" "${pname}-${version}"}
   '';
 
-  postFixup = ''
-    ln -sf $out/lib/libdmr.so $dev/lib/libdmr.so
-  '';
-
   meta = with lib; {
-    description = "A full-featured video player supporting playing local and streaming media in multiple video formats";
+    description = "Full-featured video player supporting playing local and streaming media in multiple video formats";
     homepage = "https://github.com/linuxdeepin/deepin-movie-reborn";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
