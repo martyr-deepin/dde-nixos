2872a1cb1fea011a6b6122a5d74e8fca58d3cbcd
rewine <luhongxu@uniontech.com>
2022-09-06 11:29:09 +0800

    dtkcommon: 5.5.23 -> 5.5.23+

--- packages/library/dtkcommon/default.nix
+++ packages/library/dtkcommon/default.nix
@@ -2,7 +2,7 @@
 , lib
 , fetchFromGitHub
 , pkgconfig
-, qmake
+, cmake
 , qtbase
 , qttools
 , wrapQtAppsHook
@@ -11,41 +11,27 @@
 
 stdenv.mkDerivation rec {
   pname = "dtkcommon";
-  version = "5.5.23";
+  version = "5.5.23+";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-6Oe0b74Vkj9OQMVh5kQlx4FVO66HSL0Y1RJkCFqeqFQ=";
+    rev = "a070bf688aeccd4cb701b078897fcd3efe88a657";
+    sha256 = "sha256-B0szFtXsEhcvPuwVy8SIDVKvJCPk2V4/N0lsNR0rjD4=";
   };
 
   nativeBuildInputs = [
-    qmake
+    cmake
     pkgconfig
     qttools
     wrapQtAppsHook
   ];
 
-  buildInputs = [
-    qtbase
-  ];
+  buildInputs = [ qtbase ];
 
   qmakeFlags = [ "PREFIX=${placeholder "out"}" ];
 
   patches = [ ./0001-dtk_lib-disable-examples-subdirs.patch ];
-
-  postPatch = ''
-    substituteInPlace dtkcommon.pro \
-      --replace '$${getQtMacroFromQMake(QT_INSTALL_LIBS)}'      $out/lib \
-      --replace '$${getQtMacroFromQMake(QT_INSTALL_ARCHDATA)}'  $out
-    substituteInPlace cmake/Dtk/DtkInstallDConfigConfig.cmake \
-      --replace '/usr/share/dsg' 'share/dsg' \
-      --replace '/opt/apps'      'opt/apps'
-    substituteInPlace features/dtk_install_dconfig.prf \
-      --replace '/usr/share/dsg' 'share/dsg' \
-      --replace '/opt/apps'      'opt/apps'
-  '';
 
   preFixup = ''
     glib-compile-schemas ${glib.makeSchemaPath "$out" "${pname}-${version}"}
