23276d47b8bbf9bf103c5b56980e8c177b732963
rewine <luhongxu@uniontech.com>
2022-09-07 17:46:15 +0800

    deepin-desktop-schemas: 5.10.6 -> 5.10.11

--- packages/go-package/deepin-desktop-schemas/default.nix
+++ packages/go-package/deepin-desktop-schemas/default.nix
@@ -7,38 +7,9 @@
 , go-lib
 , glib
 }:
-let
-  sw_share = [ "/usr/share" "/run/current-system/sw/share" ];
-  patchList = {
-    "schemas/com.deepin.dde.appearance.gschema.xml" = [
-      sw_share
-      # "/usr/share/backgrounds/default_background.jpg"
-    ];
-    "schemas/wrap/com.deepin.wrap.gnome.desktop.background.gschema.xml" = [
-      sw_share
-      # /usr/share/backgrounds/gnome/adwaita-timed.xml
-    ];
-    "schemas/wrap/com.deepin.wrap.gnome.desktop.screensaver.gschema.xml" = [
-      sw_share
-      # /usr/share/backgrounds/gnome/adwaita-lock.jpg
-    ];
-    "schemas/wrap/com.deepin.wrap.gnome.desktop.app-folders.gschema.xml" = [
-      sw_share
-      # /usr/share/desktop-directories.
-    ];
-    "overrides/mips/appearance.override" = [
-      sw_share
-      # /usr/share/wallpapers/deepin/desktop.bmp
-    ];
-    "overrides/common/com.deepin.wrap.gnome.desktop.override" = [
-      sw_share
-      # /usr/share/backgrounds/default_background.jpg
-    ];
-  };
-in
 buildGoPackage rec {
   pname = "deepin-desktop-schemas";
-  version = "5.10.6";
+  version = "5.10.11";
 
   goPackagePath = "github.com/linuxdeepin/deepin-desktop-schemas";
 
@@ -46,13 +17,20 @@ buildGoPackage rec {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-fCI9u0eotgaay/Ci8/3bzr4MZ95GN5fH0LBXFKHJlF8=";
+    sha256 = "sha256-MboNj0zC3azavDUsmeNNafCcUa0GeoySl610+WOtNww=";
   };
 
   nativeBuildInputs = [ glib ];
   buildInputs = [ go-lib ];
 
-  postPatch = getPatchFrom patchList;
+  postPatch = ''
+    # Relocate files path for backgrounds and wallpapers
+    for file in $(grep -rl "/usr/share")
+    do
+      substituteInPlace $file \
+        --replace "/usr/share" "/run/current-system/sw/share"
+    done
+  '';
 
   buildPhase = ''
     runHook preBuild
