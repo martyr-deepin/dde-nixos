ec3a058096469ad3729715ba87a8287a9f3e0d59
rewine <luhongxu@uniontech.com>
2022-09-13 13:45:16 +0800

    startdde: update patch

--- packages/go-package/startdde/default.nix
+++ packages/go-package/startdde/default.nix
@@ -24,12 +24,11 @@
 , dde-session-shell
 , gnome
 , dde-kwin
+, pciutils
 }:
 let
   patchList = {
     "main.go" = [
-      [ "/usr/bin/kwin_no_scale" "${dde-kwin}/bin/kwin_no_scale" ]
-      [ "/usr/lib/deepin-daemon/dde-session-daemon" "${dde-daemon}/lib/deepin-daemon/dde-session-daemon" ]
       [ "/usr/bin/dde-dock" "dde-dock" ]
       [ "/usr/bin/dde-desktop" "dde-desktop" ]
       [ "/usr/libexec/deepin/login-reminder-helper" "login-reminder-helper" ]
@@ -38,50 +37,19 @@ let
     "session.go" = [
       [ "/usr/share/applications/dde-lock.desktop" "${dde-session-shell}/share/applications/dde-lock.desktop" ]
       [ "/usr/bin/dde-shutdown" "dde-shutdown" ]
-      [ "/usr/lib/deepin-daemon/langselector" "${dde-daemon}/lib/deepin-daemon/langselector" ]
       [ "/usr/bin/dde-lock" "${dde-session-shell}/bin/dde-lock" ]
-      [ "/usr/lib/deepin-daemon/dde-osd" "${dde-session-ui}/lib/deepin-daemon/dde-osd" ]
       [ "/usr/bin/gnome-keyring-daemon" "${gnome.gnome-keyring}/bin/gnome-keyring-daemon" ]
       #? [ "/usr/share/deepin-default-settings/fontconfig.json"  ] 
-      [ "/bin/bash" "${runtimeShell}" ]
-    ];
-    "main_test.go" = [
-      [ "/usr/bin/kwin_no_scale" "${dde-kwin}/bin/kwin_no_scale" ]
     ];
     "misc/lightdm.conf" = [
       # "/usr/sbin/deepin-fix-xauthority-perm" 
     ];
-    "misc/Xsession.d/00deepin-dde-env" = [
-      # "/usr/bin/startdde
-    ];
     "misc/auto_launch/chinese.json" = [
       [ "/usr/bin/dde-file-manager" "dde-file-manager" ]
-      [ "/usr/lib/polkit-1-dde/dde-polkit-agent" "${dde-polkit-agent}/lib/polkit-1-dde/dde-polkit-agent" ]
       [ "/usr/bin/dde-shutdown" "dde-shutdown" ]
-    ];
-    "misc/auto_launch/default.json" = [
-      [ "/usr/lib/polkit-1-dde/dde-polkit-agent" "${dde-polkit-agent}/lib/polkit-1-dde/dde-polkit-agent" ]
-    ];
-    "utils.go" = [
-      [ "/usr/lib/deepin-daemon/dde-welcome" "${dde-session-ui}/lib/deepin-daemon/dde-welcome" ]
     ];
     "launch_group.go" = [
       # "/usr/share/startdde/auto_launch.json" 
-    ];
-    "watchdog/deepinid_daemon.go" = [
-      [ "/usr/lib/deepin-deepinid-daemon/deepin-deepinid-daemon" "deepin-deepinid-daemon" ]
-    ];
-    "watchdog/watchdog_test.go" = [
-      [ "/usr/bin/kwin_no_scale" "${dde-kwin}/bin/kwin_no_scale" ]
-    ];
-    "watchdog/dde_polkit_agent.go" = [
-      [ "/usr/lib/polkit-1-dde/dde-polkit-agent" "${dde-polkit-agent}/lib/polkit-1-dde/dde-polkit-agent" ]
-    ];
-    "launch_group_test.go" = [
-      [ "/usr/lib/polkit-1-dde/dde-polkit-agent" "${dde-polkit-agent}/lib/polkit-1-dde/dde-polkit-agent" ]
-    ];
-    "testdata/auto_launch/auto_launch.json" = [
-      [ "/usr/lib/polkit-1-dde/dde-polkit-agent" "${dde-polkit-agent}/lib/polkit-1-dde/dde-polkit-agent" ]
     ];
     "testdata/desktop/dde-file-manager.desktop" = [
       [ "/usr/bin/dde-file-manager" "dde-file-manager" ]
@@ -89,15 +57,20 @@ let
     "memchecker/config.go" = [
       # /usr/share/startdde/memchecker.json 
     ];
-    "display/manager.go" = [
-      [ "/usr/lib/deepin-daemon/dde-touchscreen-dialog" "dde-touchscreen-dialog" ]
-      [ "/bin/bash" "${runtimeShell}" ]
-    ];
     "display/wayland.go" = [
       [ "/usr/bin/dde_wloutput" "dde_wloutput" ]
     ];
     #?  "memanalyzer/config_test.go" 
   };
+  replaceAll = x: y: ''
+    echo Replacing "${x}" to "${y}":
+    for file in $(grep -rl "${x}")
+    do
+      echo -- $file
+      substituteInPlace $file \
+        --replace "${x}" "${y}"
+    done
+  '';
 in
 buildGoPackage rec {
   pname = "startdde";
@@ -112,10 +85,17 @@ buildGoPackage rec {
     sha256 = "sha256-qS7r7CeK8ogrxqiNJh4/fYAaLo2j3f+N4S9a6jiur+U=";
   };
 
-  postPatch = getPatchFrom patchList + ''
+  postPatch = getPatchFrom patchList
+    + replaceAll "/bin/bash" "${runtimeShell}"
+    + replaceAll "/bin/sh" "${runtimeShell}"
+    + replaceAll "/usr/bin/kwin_no_scale" "${dde-kwin}/bin/kwin_no_scale"
+    + replaceAll "/usr/lib/deepin-daemon" "/run/current-system/sw/lib/deepin-daemon"
+    + replaceAll "/usr/lib/polkit-1-dde/dde-polkit-agent" "${dde-polkit-agent}/lib/polkit-1-dde/dde-polkit-agent"
+    + replaceAll "/usr/bin/startdde" "$out/bin/startdde"
+    + replaceAll "\"lspci\"" "\"${pciutils}/bin/lspci\""
+    + ''
     substituteInPlace "startmanager.go"\
-      --replace "/usr/share/startdde/app_startup.conf" "$out/share/startdde/app_startup.conf" \
-      --replace "/bin/sh" "${runtimeShell}"
+      --replace "/usr/share/startdde/app_startup.conf" "$out/share/startdde/app_startup.conf"
     substituteInPlace misc/xsessions/deepin.desktop.in --subst-var-by PREFIX $out
   '';
 
@@ -163,7 +143,7 @@ buildGoPackage rec {
 
   meta = with lib; {
     description = "starter of deepin desktop environment";
-    longDescription = "launching DDE components and invoking user's custom applications which compliant with xdg autostart specification";
+    longDescription = "Startdde is used for launching DDE components and invoking user's custom applications which compliant with xdg autostart specification";
     homepage = "https://github.com/linuxdeepin/startdde";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
