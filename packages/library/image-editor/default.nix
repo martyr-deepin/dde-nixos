2c7d4c25ebc91a20e225dece91d2a50280d403a2
rewine <luhongxu@uniontech.com>
2022-09-14 17:02:51 +0800

    image-editor: fix patch

--- packages/library/image-editor/default.nix
+++ packages/library/image-editor/default.nix
@@ -10,34 +10,33 @@
 , opencv
 , freeimage
 , libmediainfo
+, ffmpegthumbnailer
 , pcre
 }:
 
 stdenv.mkDerivation rec {
   pname = "image-editor";
-  version = "1.0.19";
+  version = "1.0.21";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-fAmpgR8ouW04iYLLBsu2n/T9Sy8Q5lSNSSW3HIvSw/Q=";
+    rev = "3ced483348484b63350d4ee78ba7dd053c0c8cb6";
+    sha256 = "sha256-EODxC2348H4jVTPHgbfVQm6bHMnNFL9u4l+pdWwNTvU=";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "feat_check_PREFIX_value_before_set";
-      url = "https://github.com/linuxdeepin/image-editor/commit/d01339a04850d6b88c5b9b314a6bae3b32ecf52a.patch";
-      sha256 = "sha256-Tozvy4YCvvPeYaSdnFcQKxsXGHjJ127k3XmzsMHMRtU=";
-    })
-  ];
-
-  postPatch = ''
-    substituteInPlace libimageviewer/CMakeLists.txt \
-      --replace "/usr/lib/" "$out/lib/"
-    # cat libimageviewer/CMakeLists.txt
-  '';
-  #TODO:
+  # patches = [
+  #   (fetchpatch {
+  #     name = "feat_check_PREFIX_value_before_set";
+  #     url = "https://github.com/linuxdeepin/image-editor/commit/dae86e848cf53ba0ece879d81e8d5335d61a7473.patch";
+  #     sha256 = "sha256-lxmR+nIrMWVyhl1jpA17x2yqJ40h5vnpqKKcjd8j9RY=";
+  #   })
+  #   (fetchpatch {
+  #     name = "feat: use FULL install path";
+  #     url = "https://github.com/linuxdeepin/image-editor/commit/855ae53a0444ac628aa0fe893932df6263b82e2e.patch";
+  #     sha256 = "sha256-3Dynlwl/l/b6k6hOHjTdoDQ/VGBDfyRz9b8QY8FEsCc=";
+  #   })
+  # ];
 
   nativeBuildInputs = [ cmake pkgconfig qttools wrapQtAppsHook ];
 
@@ -46,10 +45,13 @@ stdenv.mkDerivation rec {
     opencv
     freeimage
     libmediainfo
+    ffmpegthumbnailer
     pcre
   ];
 
-  cmakeFlags = [ "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}" "-DCMAKE_INSTALL_LIBDIR=lib" ];
+  cmakeFlags = [
+    "-DVERSION=${version}"
+  ];
 
   meta = with lib; {
     description = "image editor lib for dtk";
