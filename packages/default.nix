157a2775b7038d3d66554264cb5f248adf3501d7
rewine <luhongxu@uniontech.com>
2022-10-12 09:28:05 +0800

    ffmpegthumbnailer: remove

--- packages/default.nix
+++ packages/default.nix
@@ -63,7 +63,6 @@ let
     qt5integration = callPackage ./library/qt5integration { };
     docparser = callPackage ./library/docparser { };
     dtk = [ dtkcommon dtkcore dtkgui dtkwidget /*qt5integration*/ qt5platform-plugins ];
-    ffmpegthumbnailer = callPackage ./library/ffmpegthumbnailer { };
 
     #### MISC
     dde-polkit-agent = callPackage ./misc/dde-polkit-agent { };
