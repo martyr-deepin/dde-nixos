a5999e97e455aec559bbcc75a9a40837bb1f92a5
rewine <luhongxu@uniontech.com>
2022-10-21 13:44:23 +0800

    dde-app-services: 0.0.16 -> 0.0.19

--- packages/misc/dde-app-services/default.nix
+++ packages/misc/dde-app-services/default.nix
@@ -12,37 +12,23 @@
 }:
 let
   patchList = {
-    ### MISC
     "dconfig-center/dde-dconfig-daemon/services/org.desktopspec.ConfigManager.service" = [ ];
-
-    ### CODE
-    "dconfig-center/dde-dconfig-editor/oemdialog.cpp" = [
-      [ "/etc/dsg" "/run/current-system/sw/share/dsg" ]
-    ];
     "dconfig-center/dde-dconfig-daemon/dconfig_global.h" = [
-      [ "/etc/dsg" "/run/current-system/sw/share/dsg" ]
+      # "/etc/dsg" 
       [ "/usr/share/dsg" "/run/current-system/sw/share/dsg" ]
     ];
   };
 in
 stdenv.mkDerivation rec {
   pname = "dde-app-services";
-  version = "0.0.16";
+  version = "0.0.19";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-iPYjXWBSc9p8AwyylDEeIpnOpPynvnYlPhLhA1MP1fY=";
+    sha256 = "sha256-cB9mA99eKx8NrBBIUfDr/jyLUfWmwaW7m3ssm22iCLs=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "chore: use GNUInstallDirs in CmakeLists";
-      url = "https://github.com/linuxdeepin/dde-app-services/commit/f6df6915fda2d360e44f0c941cc5dd7cece24ef0.patch";
-      sha256 = "sha256-M1LjYRbkrngOKUqo7R5u0Gkf7CA+tvlNvx04RBVDE+g=";
-    })
-  ];
 
   nativeBuildInputs = [
     cmake
