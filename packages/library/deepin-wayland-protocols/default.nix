4b6b22921d7ec8e4fb47b44b01249e08fdde0a7e
rewine <lhongxu@outlook.com>
2022-11-25 15:37:50 +0800

    deepin-wayland-protocols: fix at 1.6.0-deepin.1.1

--- packages/library/deepin-wayland-protocols/default.nix
+++ packages/library/deepin-wayland-protocols/default.nix
@@ -1,23 +1,9 @@
 { stdenv
-, stdenvNoCC
 , lib
-, fetchpatch
-, getUsrPatchFrom
-, pkg-config
 , fetchFromGitHub
 , cmake
-, wayland
-, qtbase
-, qttools
-, wrapQtAppsHook
 , extra-cmake-modules
-, plasma-wayland-protocols
 }:
-let
-  patchList = {
-  };
-
-in
 stdenv.mkDerivation rec {
   pname = "deepin-wayland-protocols";
   version = "1.6.0-deepin.1.1";
@@ -26,41 +12,18 @@ stdenv.mkDerivation rec {
     owner = "justforlxz";
     repo = pname;
     rev = version;
-    sha256 = "";
+    sha256 = "sha256-OW3Eiu0apa3sRrjet4bRUg6vsMycH6oejePZCxznH6Y=";
   };
-
-  patches = [
-  ];
-
-  postPatch = getUsrPatchFrom patchList + ''
-    patchShebangs .
-  '';
 
   nativeBuildInputs = [
     cmake
     extra-cmake-modules
   ];
 
-  # buildInputs = [ plasma-wayland-protocols wayland wayland-protocols ];
-  
-  propagatedBuildInputs = [ qtbase ];
-
-  NIX_CFLAGS_COMPILE = [
-  ];
-
-  cmakeFlags = [
-  ];
-
-  postFixup = ''
-    wrapProgram $out/bin/kwin_no_scale \
-      --set QT_QPA_PLATFORM_PLUGIN_PATH "${placeholder "out"}/${qtbase.qtPluginPrefix}"
-  '';
-  ## FIXME: why cann't use --prefix
-
   meta = with lib; {
-    description = "KWin configuration for Deepin Desktop Environment";
-    homepage = "https://github.com/linuxdeepin/dde-kwin";
-    license = licenses.gpl3Plus;
+    description = "Xml files of the non-standard wayland protocols use in deepin";
+    homepage = "https://github.com/linuxdeepin/deepin-wayland-protocols";
+    license = licenses.lgpl21Plus;
     platforms = platforms.linux;
   };
 }
