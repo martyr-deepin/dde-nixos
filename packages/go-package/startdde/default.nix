97b944cd0af6a8b23ee9fd052cf44b68ba396b05
rewine <lhongxu@outlook.com>
2022-11-28 10:01:56 +0800

    startdde: fix path

--- packages/go-package/startdde/default.nix
+++ packages/go-package/startdde/default.nix
@@ -23,6 +23,7 @@
 , dde-polkit-agent
 , dde-session-ui
 , dde-session-shell
+, dde-kwin
 , gnome
 , pciutils
 }:
@@ -81,7 +82,7 @@ buildGoPackage rec {
 
   postPatch = replaceAll "/bin/bash" "${runtimeShell}"
     + replaceAll "/bin/sh" "${runtimeShell}"
-    + replaceAll "/usr/bin/kwin_no_scale" "kwin_no_scale"
+    + replaceAll "/usr/bin/kwin_no_scale" "${dde-kwin}/bin/kwin_no_scale"
     + replaceAll "/usr/lib/deepin-daemon" "/run/current-system/sw/lib/deepin-daemon"
     + replaceAll "/usr/lib/polkit-1-dde/dde-polkit-agent" "${dde-polkit-agent}/lib/polkit-1-dde/dde-polkit-agent"
     + replaceAll "/usr/bin/startdde" "$out/bin/startdde"
