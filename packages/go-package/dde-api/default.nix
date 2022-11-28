b202b002f66eb6b8afa0e25658975695b5dba12c
rewine <lhongxu@outlook.com>
2022-11-28 15:58:25 +0800

    dde-api: 5.5.31 -> 5.5.32

--- packages/go-package/dde-api/default.nix
+++ packages/go-package/dde-api/default.nix
@@ -29,41 +29,25 @@
 , coreutils
 }:
 let
+  share_to_sw = [ "/usr/share" "/run/current-system/sw/share" ];
   patchList = {
-    ### CODE
-    "lunar-calendar/huangli.go" = [
-      # /usr/share/dde-api/data/huangli*
-    ];
+    "lunar-calendar/huangli.go" = [ ];
+    "adjust-grub-theme/main.go" = [ ];
     "locale-helper/main.go" = [
       [ "/usr/local/sbin:" "PATH:/usr/local/sbin" ]
     ];
-
     "device/main.go" = [
       [ "/usr/local/sbin:" "PATH:/usr/local/sbin" ]
     ];
-    "i18n_dependent/i18n_dependent.go" = [
-      [ "/usr/share" "/run/current-system/sw/share" ]
-    ];
-    "adjust-grub-theme/main.go" = [
-      # "/usr/share/dde-api/data/grub-themes/"
-    ];
-    "sound-theme-player/main.go" = [
-      [ "/usr/sbin/alsactl" "alsactl" ]
-    ];
-    "themes/theme.go" = [
-      [ "/usr/share" "/run/current-system/sw/share" ]
-    ];
-    "themes/settings.go" = [
-      [ "/usr/share" "/run/current-system/sw/share" ]
-    ];
-    "lang_info/lang_info.go" = [
-      [ "/usr/share" "/run/current-system/sw/share" ]
-    ];
+    "i18n_dependent/i18n_dependent.go" = [ share_to_sw ];
+    "themes/theme.go" = [ share_to_sw ];
+    "themes/settings.go" = [ share_to_sw ];
+    "lang_info/lang_info.go" = [ share_to_sw ];
   };
 in
 buildGoPackage rec {
   pname = "dde-api";
-  version = "5.5.31";
+  version = "5.5.32";
 
   goPackagePath = "github.com/linuxdeepin/dde-api";
 
@@ -71,12 +55,13 @@ buildGoPackage rec {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-FiM1iBPgcEbgLQwZz6cwOrdGCzRz0Yeq2ZbbnpuTjU0";
+    sha256 = "sha256-F+vEOSpysqVtjs8de5mCmeANuCbYUQ860ZHl5rwNYac=";
   };
 
   postPatch = replaceAll "/usr/lib/deepin-api" "/run/current-system/sw/lib/deepin-api"
       + replaceAll "/usr/bin/dbus-send" "${dbus}/bin/dbus-send"
       + replaceAll "/usr/bin/true" "${coreutils}/bin/true"
+      + replaceAll "/usr/sbin/alsactl" "alsactl"
       + replaceAll "/usr/share/i18n" "/run/current-system/sw/share/i18n"
       + getUsrPatchFrom patchList;
 
@@ -129,7 +114,7 @@ buildGoPackage rec {
   '';
 
   meta = with lib; {
-    description = "Provides some dbus interfaces that is used for screen zone detecting, thumbnail generating, sound playing, etc";
+    description = "Dbus interfaces used for screen zone detecting, thumbnail generating, sound playing, etc";
     homepage = "https://github.com/linuxdeepin/dde-api";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
