3cd06621b09bb2d2b5caa6f90cc90e5a0dea7f4b
rewine <lhongxu@outlook.com>
2022-12-10 14:58:40 +0800

    dwayland: 5.24.3-deepin.1.3 -> 5.24.3-deepin.1.4

--- packages/library/dwayland/default.nix
+++ packages/library/dwayland/default.nix
@@ -8,21 +8,23 @@
 , wayland-protocols
 , extra-cmake-modules
 , deepin-wayland-protocols
+, qttools
 }:
 stdenv.mkDerivation rec {
   pname = "dwayland";
-  version = "5.24.3-deepin.1.3";
+  version = "5.24.3-deepin.1.4";
 
   src = fetchFromGitHub {
     owner = "justforlxz";
     repo = pname;
     rev = version;
-    sha256 = "sha256-87Ih2IiOFGF4pXdZH+4VLa1c59PuuFuX3IEGuCmtKzA=";
+    sha256 = "sha256-/aWS4uvhxi9azxJWjRE+Bw+veURFO+mC8l9yypseclU=";
   };
 
   nativeBuildInputs = [
     cmake
     extra-cmake-modules
+    qttools
   ];
 
   buildInputs = [
