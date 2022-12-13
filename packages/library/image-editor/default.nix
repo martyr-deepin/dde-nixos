6f87cba781e85568e77cc490c5ddec5550ba0247
rewine <luhongxu@deepin.org>
2022-12-13 11:05:56 +0800

    image-editor: 1.0.21 -> 1.0.24

--- packages/library/image-editor/default.nix
+++ packages/library/image-editor/default.nix
@@ -16,27 +16,35 @@
 
 stdenv.mkDerivation rec {
   pname = "image-editor";
-  version = "1.0.21";
+  version = "1.0.24";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
-    rev = "3ced483348484b63350d4ee78ba7dd053c0c8cb6";
-    sha256 = "sha256-EODxC2348H4jVTPHgbfVQm6bHMnNFL9u4l+pdWwNTvU=";
+    rev = version;
+    sha256 = "sha256-0p/aicuBfaEyvfZomnz49IJLospWIdl23ZreDe+Bzqc=";
   };
 
-  # patches = [
-  #   (fetchpatch {
-  #     name = "feat_check_PREFIX_value_before_set";
-  #     url = "https://github.com/linuxdeepin/image-editor/commit/dae86e848cf53ba0ece879d81e8d5335d61a7473.patch";
-  #     sha256 = "sha256-lxmR+nIrMWVyhl1jpA17x2yqJ40h5vnpqKKcjd8j9RY=";
-  #   })
-  #   (fetchpatch {
-  #     name = "feat: use FULL install path";
-  #     url = "https://github.com/linuxdeepin/image-editor/commit/855ae53a0444ac628aa0fe893932df6263b82e2e.patch";
-  #     sha256 = "sha256-3Dynlwl/l/b6k6hOHjTdoDQ/VGBDfyRz9b8QY8FEsCc=";
-  #   })
-  # ];
+  patches = [
+    (fetchpatch {
+      name = "feat_check_PREFIX_value_before_set";
+      url = "https://github.com/linuxdeepin/image-editor/commit/dae86e848cf53ba0ece879d81e8d5335d61a7473.patch";
+      sha256 = "sha256-lxmR+nIrMWVyhl1jpA17x2yqJ40h5vnpqKKcjd8j9RY=";
+    })
+    (fetchpatch {                                                                    
+      name = "feat: use FULL install path";                                          
+      url = "https://github.com/linuxdeepin/image-editor/commit/855ae53a0444ac628aa0fe893932df6263b82e2e.patch";                                                            
+      sha256 = "sha256-3Dynlwl/l/b6k6hOHjTdoDQ/VGBDfyRz9b8QY8FEsCc=";                
+    })
+  ];
+
+  postPatch = ''
+    substituteInPlace libimageviewer/service/ffmpegvideothumbnailer.cpp \
+        --replace 'libPath("libffmpegthumbnailer.so")'  'QString("${ffmpegthumbnailer.out}/lib/libffmpegthumbnailer.so")'
+
+    substituteInPlace libimageviewer/CMakeLists.txt --replace '/usr' '$out'
+    substituteInPlace libimagevisualresult/CMakeLists.txt --replace '/usr' '$out'
+  '';
 
   nativeBuildInputs = [ cmake pkg-config qttools wrapQtAppsHook ];
 
