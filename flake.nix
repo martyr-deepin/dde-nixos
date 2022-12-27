e851eb84a803e58590ddd9b9406890f898075c3d
rewine <lhongxu@outlook.com>
2022-12-27 18:11:41 +0800

    Environment use attribute set

--- flake.nix
+++ flake.nix
@@ -379,7 +379,7 @@
                       ExecStart = "${packages.deepin-anything}/bin/deepin-anything-monitor";
                       ExecStartPre = "modprobe vfs_monitor";
                       ExecStopPost = "rmmod vfs_monitor";
-                      Environment = "DAS_DEBUG_PLUGINS=1";
+                      Environment = [ "DAS_DEBUG_PLUGINS=1" ];
                       Restart = "always";
                       RestartSec = 10;
                     };
