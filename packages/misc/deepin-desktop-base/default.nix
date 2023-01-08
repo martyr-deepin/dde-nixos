7e7a1248edfa56a12f5c657d93b5653d87c197d2
rewine <luhongxu@deepin.org>
2023-01-08 16:47:53 +0800

    deepin-desktop-base: remove some svg

--- packages/misc/deepin-desktop-base/default.nix
+++ packages/misc/deepin-desktop-base/default.nix
@@ -16,24 +16,21 @@ stdenvNoCC.mkDerivation rec {
   makeFlags = [ "DESTDIR=${placeholder "out"}" ];
 
   postInstall = ''
-    rm $out/etc/lsb-release
-    rm -r $out/etc/systemd
+    rm -r $out/etc
     rm -r $out/usr/share/python-apt
     rm -r $out/usr/share/plymouth
     rm -r $out/usr/share/distro-info
     mv $out/usr/* $out/
     rm -r $out/usr
 
-    install -D ${./distribution_logo.svg} $out/share/pixmaps/distribution_logo.svg
-    install -D ${./distribution_logo_light.svg} $out/share/pixmaps/distribution_logo_light.svg
     install -D ${./distribution_logo_transparent.svg} $out/share/pixmaps/distribution_logo_transparent.svg
-    ln -s $out/lib/deepin/desktop-version $out/etc/deepin-version
   '';
+  # distribution_logo_transparent.svg come form nixos-artwork/logo/nixos-white.svg, used for dde-lock
 
   meta = with lib; {
     description = "Base assets and definitions for Deepin Desktop Environment";
     homepage = "https://github.com/linuxdeepin/deepin-desktop-base";
-    license = licenses.gpl3Plus;
+    license = licenses.gpl3Plus; 
     platforms = platforms.linux;
   };
 }
