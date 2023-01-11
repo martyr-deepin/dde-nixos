04dbaa70f53d5cc0498579c197c1298bb3fd0c1e
rewine <lhongxu@outlook.com>
2023-01-11 09:28:35 +0800

    vm: dont full install

--- vm/flake.nix
+++ vm/flake.nix
@@ -57,7 +57,7 @@
           #desktopManager.plasma5.enable = true;
           desktopManager.deepin = {
             enable = true;
-            full = true;
+            full = false;
           };
 
         };
