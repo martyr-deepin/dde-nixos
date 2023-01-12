a4c75a6c66a9774d913fa4d3c71427b66aa1ae80
rewine <luhongxu@deepin.org>
2023-01-12 14:49:35 +0800

    dde-polkit-agent: 5.5.21 -> 5.5.22

--- packages/core/dde-polkit-agent/default.nix
+++ packages/core/dde-polkit-agent/default.nix
@@ -4,35 +4,29 @@
 , getUsrPatchFrom
 , dtk
 , qt5integration
-, qt5platform-plugins
 , dde-qt-dbus-factory
 , pkg-config
 , cmake
 , qttools
 , wrapQtAppsHook
 , polkit-qt
-, dde-session-shell
 , qtbase
 }:
-let
-  patchList = {
-    "AuthDialog.cpp" = [
-      [ "/usr/share/dde-session-shell/dde-session-shell.conf" "${dde-session-shell}/share/dde-session-shell/dde-session-shell.conf" ]
-    ];
-  };
-in
 stdenv.mkDerivation rec {
   pname = "dde-polkit-agent";
-  version = "5.5.21";
+  version = "5.5.22";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-jWH1E3lBEIlLtwO8oTIaK7CzUhGfxJIo2KOQ1om2qco=";
+    sha256 = "sha256-4wAqMymLPxKXbfAx2PtkEGfwenphPxBZn+qCdcyqNc0=";
   };
 
-  postPatch = getUsrPatchFrom patchList;
+  postPatch = ''
+    substituteInPlace AuthDialog.cpp \
+      --replace "/usr/share/dde-session-shell/dde-session-shell.conf" "/etc/dde-session-shell/dde-session-shell.conf"
+  '';
 
   nativeBuildInputs = [
     cmake
