ad88becc2c11db3d20eea0c74108a00d65bd194e
rewine <luhongxu@deepin.org>
2023-02-27 11:32:05 +0800

    dde-daemon: better patch

--- packages/go-package/dde-daemon/default.nix
+++ packages/go-package/dde-daemon/default.nix
@@ -1,18 +1,18 @@
 { stdenv
 , lib
 , fetchFromGitHub
-, getUsrPatchFrom
 , replaceAll
-, substituteAll
+, getUsrPatchFrom
 , buildGoPackage
 , pkg-config
+, deepin-gettext-tools
+, gettext
+, python3
+, wrapGAppsHook
 , go-dbus-factory
 , go-gir-generator
 , go-lib
-, deepin-gettext-tools
-, gettext
 , dde-api
-, deepin-desktop-schemas
 , alsa-lib
 , glib
 , gtk3
@@ -23,16 +23,10 @@
 , linux-pam
 , networkmanager
 , pulseaudio
-, python3
-, hicolor-icon-theme
 , glibc
 , gdk-pixbuf-xlib
 , tzdata
-, go
-, makeWrapper
 , xkeyboard_config
-, wrapGAppsHook
-, wrapQtAppsHook
 , runtimeShell
 , fprintd
 , xorg
@@ -42,10 +36,9 @@
 , util-linux
 , ddcutil
 , libxcrypt
-, dde-account-faces
 }:
 let
-  goCodePatchs = {
+  patchList = {
     "gesture/config.go" = [
       # "/usr/share/dde-daemon/gesture.json"
     ];
@@ -57,7 +50,6 @@ let
       [ "/usr/share" "/run/current-system/sw/share" ]
       # return []string{"/usr/share", "/usr/local/share"}
     ];
-
     "appearance/ifc.go" = [ ];
     "launcher/manager.go" = [
       [ "/usr/share" "/run/current-system/sw/share" ]
@@ -65,30 +57,27 @@ let
     ];
     "launcher/manager_init.go" = [
       [ "/usr/share" "/run/current-system/sw/share" ]
-      # "/usr/share/applications"
+      # /usr/share/applications
     ];
     "audio/audio_config.go" = [
-      # "/usr/share/dde-daemon/audio/echoCancelEnable.sh"
+      # /usr/share/dde-daemon/audio/echoCancelEnable.sh
     ];
-    # "bin/dde-authority/fprint_transaction.go" = [
-    #   #? fprintd 0.8 in deepin but 1.9in nixos
-    #   # services.fprintd.enable
-    #   [ "/usr/lib/fprintd/fprintd" "${fprintd}/libexec/fprintd" ]
-    # ];
+    "bin/dde-authority/fprint_transaction.go" = [
+      # fprintd 0.8 in deepin but 1.9 in nixos
+      [ "/usr/lib/fprintd/fprintd" "${fprintd}/libexec/fprintd" ]
+    ];
     "system/gesture/config.go" = [
       [ "/usr/share" "/run/current-system/sw/share" ]
     ];
-    #"system/power/manager_lmt.go" = [
-    #  "/usr/sbin/laptop_mode" 
-    #  TODO https://github.com/rickysarraf/laptop-mode-tools
-    #];
+    "system/power/manager_lmt.go" = [
+      [ "/usr/sbin/laptop_mode" "laptop_mode" ]
+    ];
     "bin/user-config/config_datas.go" = [
       [ "/usr/share" "/run/current-system/sw/share" ]
-      # "/usr/share/doc/deepin-manuals"
-      # "/usr/share/deepin-sample-music"
+      # /usr/share/doc/deepin-manuals /usr/share/deepin-sample-music
     ];
     "bin/dde-system-daemon/virtual.go" = [
-      # "/usr/share/dde-daemon/supportVirsConf.ini"
+      # /usr/share/dde-daemon/supportVirsConf.ini
     ];
     "system/display/displaycfg.go" = [
       [ "/usr/bin/lightdm-deepin-greeter" "lightdm-deepin-greeter" ]
@@ -96,44 +85,41 @@ let
     ];
     "dock/identify_window.go" = [
       [ "/usr/share" "/run/current-system/sw/share" ]
-      # "/usr/share/applications/"
+      # /usr/share/applications/
     ];
     "keybinding/shortcuts/system_shortcut.go" = [
       [ "dbus-send" "${dbus}/bin/dbus-send" ]
       [ "gsettings" "${glib.bin}/bin/gsettings" ]
-      ## TODO
-    ];
-    "accounts/users/display_manager.go" = [
-      #? /usr/share/config/kdm/kdmrc"
     ];
     "accounts/user.go" = [
       [ "/usr/bin/dde-control-center" "dde-control-center" ]
       [ "/usr/share" "/run/current-system/sw/share" ]
-      #/usr/share/xsessions
+      # /usr/share/xsessions
     ];
     "dock/desktop_file_path.go" = [
       [ "/usr/share" "/run/current-system/sw/share" ]
-      # "/usr/share/applications/"
+      # /usr/share/applications/
     ];
     "accounts/user_chpwd_union_id.go" = [
-      [ "/usr/lib/dde-control-center/reset-password-dialog" "reset-password-dialog" ]
+      [ "/usr/lib/dde-control-center" "/run/current-system/sw/lib/dde-control-center" ]
       [ "/usr/bin/dde-control-center" "dde-control-center" ]
       [ "/usr/bin/dde-lock" "dde-lock" ]
       [ "/usr/bin/lightdm-deepin-greeter" "lightdm-deepin-greeter" ]
     ];
     "dock/dock_manager_init.go" = [
       [ "/usr/share" "/run/current-system/sw/share" ]
-      #? ddeDataDir     = "/usr/share/dde/data"
-    ];
-    "accounts/handle_event.go" = [
-      #? /usr/share/config/kdm/kdmrc
+      # ddeDataDir     = "/usr/share/dde/data"
     ];
     "accounts/manager.go" = [
       # /usr/share/dde-daemon/accounts/dbus-udcp.json
     ];
-    # "image_effect/utils.go" = [
-    #   [ "runuser" "${util-linux.bin}/bin/runuser" ]
-    # ];
+    "image_effect/utils.go" = [
+      [ "runuser" "${util-linux.bin}/bin/runuser" ]
+    ];
+    "misc/usr/share/deepin/scheduler/config.json" = [
+      [ "/usr/bin" "/run/current-system/sw/bin" ]
+      # path to deepin-anything/dde-file-manager
+    ];
   };
 in
 buildGoPackage rec {
@@ -150,12 +136,8 @@ buildGoPackage rec {
   };
 
   patches = [
-    # (substituteAll {
-    #   src = ./0001-patch_account_face_path_for_nix.patch;
-    #   actConfigDir = "\"${dde-account-faces}/share/lib/AccountsService\"";
-    # })
     ./0001-fix-wrapped-name-for-verifyExe.patch
-    ./0002-fix-PATH-when-was-launched-by-dbus.patch
+    ./0002-dont-set-PATH.diff
   ];
 
   postPatch = replaceAll "/usr/lib/deepin-api" "/run/current-system/sw/lib/deepin-api"
@@ -168,14 +150,14 @@ buildGoPackage rec {
     + replaceAll "/usr/bin/xdotool" "${xdotool}/bin/xdotool"
     + replaceAll "/usr/bin/getconf" "${getconf}/bin/getconf"
     + replaceAll "/usr/bin/dbus-send" "${dbus}/bin/dbus-send"
+    + replaceAll "/usr/share/zoneinfo" "${tzdata}/share/zoneinfo"
+    + replaceAll "/usr/share/X11/xkb/rules/base.xml" "${xkeyboard_config}/share/X11/xkb/rules/base.xml"
     + replaceAll "/usr/bin/kwin_no_scale" "kwin_no_scale"
     + replaceAll "/usr/bin/deepin-system-monitor" "deepin-system-monitor"
     + replaceAll "/usr/bin/dde-launcher" "dde-launcher"
     + replaceAll "/usr/bin/deepin-calculator" "deepin-calculator"
     + replaceAll "/usr/bin/systemd-detect-virt" "systemd-detect-virt"
-    + replaceAll "/usr/share/zoneinfo" "${tzdata}/share/zoneinfo"
-    + replaceAll "/usr/share/X11/xkb/rules/base.xml" "${xkeyboard_config}/share/X11/xkb/rules/base.xml"
-    + getUsrPatchFrom goCodePatchs + ''
+    + getUsrPatchFrom patchList + ''
     patchShebangs .
   '';
 
@@ -185,27 +167,23 @@ buildGoPackage rec {
     pkg-config
     deepin-gettext-tools
     gettext
-    networkmanager
     python3
-    makeWrapper
     wrapGAppsHook
-    wrapQtAppsHook
   ];
 
   buildInputs = [
     go-dbus-factory
     go-gir-generator
     go-lib
+    dde-api
     linux-pam
     libxcrypt
     alsa-lib
-    dde-api
-    deepin-desktop-schemas
     glib
     libgudev
     gtk3
     gdk-pixbuf-xlib
-    hicolor-icon-theme
+    networkmanager
     libinput
     libnl
     librsvg
@@ -218,24 +196,24 @@ buildGoPackage rec {
 
   buildPhase = ''
     runHook preBuild
-    GOPATH="$GOPATH:${go-dbus-factory}/share/gocode"
-    GOPATH="$GOPATH:${go-gir-generator}/share/gocode"
-    GOPATH="$GOPATH:${go-lib}/share/gocode"
-    GOPATH="$GOPATH:${dde-api}/share/gocode"
+    addToSearchPath GOPATH "${go-dbus-factory}/share/gocode"
+    addToSearchPath GOPATH "${go-gir-generator}/share/gocode"
+    addToSearchPath GOPATH "${go-lib}/share/gocode"
+    addToSearchPath GOPATH "${dde-api}/share/gocode"
     make -C go/src/${goPackagePath}
     runHook postBuild
   '';
 
   installPhase = ''
+    runHook preInstall
     make install DESTDIR="$out" PREFIX="/" -C go/src/${goPackagePath}
+    runHook postInstall
   '';
 
   postFixup = ''
     for f in "$out"/lib/deepin-daemon/*; do
       echo "Wrapping $f"
-      wrapProgram "$f" \
-        "''${gappsWrapperArgs[@]}" \
-        "''${qtWrapperArgs[@]}"
+      wrapGApp "$f"
     done
     mv $out/run/current-system/sw/lib/deepin-daemon/service-trigger $out/lib/deepin-daemon/
     rm -r $out/run
@@ -246,5 +224,6 @@ buildGoPackage rec {
     homepage = "https://github.com/linuxdeepin/dde-daemon";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
+    maintainers = teams.deepin.members;
   };
 }
