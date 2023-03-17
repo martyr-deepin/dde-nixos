aa98c0bdcdba4340b02a2e2af126d7d277213b02
rewine <lhongxu@outlook.com>
2023-03-17 23:57:59 +0800

    qt5integration: 5.6.5 -> 5.6.6

--- packages/library/qt5integration/default.nix
+++ packages/library/qt5integration/default.nix
@@ -19,22 +19,22 @@
 
 stdenv.mkDerivation rec {
   pname = "qt5integration";
-  version = "5.6.5";
+  version = "5.6.6";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-Xj8SyV5IIHxjQsdnVJQhfbw6fvCeiMckexUo+6W0GM0=";
+    sha256 = "sha256-7b18ydyy/TIEGDkFAoium1LSx3Qs4I4pYpMfehOBZbY";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "refactor: use KIconEngine instead";
-      url = "https://github.com/linuxdeepin/qt5integration/commit/fc88b79b52b1f90bdcb924269ddb5c6eccad2b99.patch";
-      sha256 = "sha256-/rZhNwba00qXkU+1Pqa3ptUlPtNNHp6DsHilc6m+ljk=";
-    })
-  ];
+  #patches = [
+  #  (fetchpatch {
+  #    name = "refactor: use KIconEngine instead";
+  #    url = "https://github.com/linuxdeepin/qt5integration/commit/fc88b79b52b1f90bdcb924269ddb5c6eccad2b99.patch";
+  #    sha256 = "sha256-/rZhNwba00qXkU+1Pqa3ptUlPtNNHp6DsHilc6m+ljk=";
+  #  })
+  #];
 
   nativeBuildInputs = [ qmake pkg-config wrapQtAppsHook ];
 
