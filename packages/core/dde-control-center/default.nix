1d98a0e4c716fe9deba496a9a94c35b18bbac715
rewine <luhongxu@deepin.org>
2023-04-20 09:25:30 +0800

    dde-control-center: 5.6.3 -> 6.0.14

--- packages/core/dde-control-center/default.nix
+++ packages/core/dde-control-center/default.nix
@@ -2,86 +2,63 @@
 , lib
 , fetchFromGitHub
 , fetchpatch
+, cmake
+, pkg-config
+, qttools
+, doxygen
+, wrapQtAppsHook
+, wrapGAppsHook
 , dtkwidget
 , qt5integration
 , qt5platform-plugins
-, dde-qt-dbus-factory
 , deepin-pw-check
-, udisks2-qt5
-, cmake
-, qttools
 , qtbase
-, pkg-config
 , qtx11extras
 , qtmultimedia
-, wrapQtAppsHook
-, wrapGAppsHook
-, gsettings-qt
-, wayland
-, kwayland
-, qtwayland
 , polkit-qt
-, pcre
 , xorg
 , libselinux
 , libsepol
 , libxcrypt
 , librsvg
-, networkmanager-qt
-, glib
 , runtimeShell
 , tzdata
 , dbus
-, gtest
 }:
 
 stdenv.mkDerivation rec {
   pname = "dde-control-center";
-  version = "5.6.3";
+  version = "6.0.14";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-/gzS+IbopIDRpufsa9cEfFBOqehPUnF4IozvwW8UEbY=";
+    sha256 = "sha256-T5L0LrKaKHgeQN/I+GZXS//Fj1EcebO+JpP8l0mjmn4=";
   };
 
   patches = [
-    # UserExperienceProgramLicenseAgreement comes from a non-open source component(deepin-deepinid-client)
-    # If we don't block it, only an empty page will be displayed here
-    # Remove this patch when dde-control-center is upgraded to 6.0.0
-    ./dont-show-endUserLicenseAgreement-for-deepinos.patch
+    #./1.patch 
   ];
 
   postPatch = ''
-    substituteInPlace src/frame/window/{mainwindow.cpp,insertplugin.cpp} com.deepin.controlcenter.develop.policy \
-      --replace "/usr/lib/dde-control-center" "/run/current-system/sw/lib/dde-control-center"
 
-    substituteInPlace src/frame/modules/datetime/timezone_dialog/timezone.cpp \
+     substituteInPlace src/plugin-datetime/window/widgets/timezone.cpp \
       --replace "/usr/share/zoneinfo" "${tzdata}/share/zoneinfo"
 
-    substituteInPlace src/frame/modules/accounts/accountsworker.cpp \
+
+    substituteInPlace src/plugin-accounts/operation/accountsworker.cpp \
       --replace "/bin/bash" "${runtimeShell}"
 
-    substituteInPlace dde-control-center-autostart.desktop \
+    substituteInPlace misc/org.deepin.dde.ControlCenter1.service \
       --replace "/usr" "$out"
-
-    substituteInPlace com.deepin.dde.ControlCenter.service \
-      --replace "/usr/bin/dbus-send" "${dbus}/bin/dbus-send" \
-      --replace "/usr/share" "$out/share"
-
-    substituteInPlace include/widgets/utils.h src/{reboot-reminder-dialog/main.cpp,frame/main.cpp,reset-password-dialog/main.cpp} \
-      --replace "/usr/share/dde-control-center" "$out/share/dde-control-center"
-
-    substituteInPlace dde-control-center-wapper \
-      --replace "qdbus" "${qttools.bin}/bin/qdbus" \
-      --replace "/usr/share" "$out/share"
   '';
 
   nativeBuildInputs = [
     cmake
-    qttools
     pkg-config
+    qttools
+    doxygen
     wrapQtAppsHook
     wrapGAppsHook
   ];
@@ -90,34 +67,30 @@ stdenv.mkDerivation rec {
   buildInputs = [
     dtkwidget
     qt5platform-plugins
-    dde-qt-dbus-factory
     deepin-pw-check
     qtbase
     qtx11extras
     qtmultimedia
-    gsettings-qt
-    udisks2-qt5
-    wayland
-    kwayland
-    qtwayland
     polkit-qt
-    pcre
-    xorg.libXdmcp
-    libselinux
-    libsepol
+    #libselinux
+    #libsepol
     libxcrypt
     librsvg
-    networkmanager-qt
-    gtest
   ];
 
   cmakeFlags = [
-    "-DCVERSION=${version}"
+    "-DDVERSION=${version}"
     "-DDISABLE_AUTHENTICATION=YES"
-    "-DDISABLE_ACTIVATOR=YES"
-    "-DDISABLE_SYS_UPDATE=YES"
-    "-DDISABLE_RECOVERY=YES"
+    "-DDISABLE_UPDATE=YES"
+    "-DDISABLE_LANGUAGE=YES"
+    "-DBUILD_DOCS=OFF"
   ];
+
+  preConfigure = ''
+    # qt.qpa.plugin: Could not find the Qt platform plugin "minimal"
+    # A workaround is to set QT_PLUGIN_PATH explicitly
+    export QT_PLUGIN_PATH=${qtbase.bin}/${qtbase.qtPluginPrefix}
+  '';
 
   # qt5integration must be placed before qtsvg in QT_PLUGIN_PATH
   qtWrapperArgs = [
