826e50c1fff13d6e281956e757abc1e05cb421d8
rewine <luhongxu@deepin.org>
2023-04-20 17:46:51 +0800

    dde-file-manager: 6.0.14 -> 6.0.15

--- packages/core/dde-file-manager/default.nix
+++ packages/core/dde-file-manager/default.nix
@@ -8,7 +8,6 @@
 , dde-qt-dbus-factory
 , docparser
 , dde-dock
-, deepin-movie-reborn
 , cmake
 , qttools
 , qtx11extras
@@ -44,13 +43,13 @@
 
 stdenv.mkDerivation rec {
   pname = "dde-file-manager";
-  version = "6.0.14";
+  version = "6.0.15";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-88Ddc3986hyFMA0bYCxiupASI+pGLqjb3igOHLA8Q/g=";
+    sha256 = "sha256-tG3Wl1AvwWhHmIIHgexv3mVVrmOwNrwn8k/sD4+WZzk";
   };
 
   nativeBuildInputs = [
@@ -61,14 +60,6 @@ stdenv.mkDerivation rec {
     wrapGAppsHook
   ];
   dontWrapGApps = true;
-
-  patches = [
-    (fetchpatch {
-      name = "feat(cmake): don't build video-preview plugin when libdmr not";
-      url = "https://github.com/linuxdeepin/dde-file-manager/commit/696c5871ad559c9b5e69b3233ef90cf32dfcf004.patch";
-      sha256 = "sha256-ykmE50KtSmhfXE6+kkDC9UBU3RLpDhkIFbDlNvM9nz8=";
-    })
-  ];
 
   postPatch = ''
     patchShebangs .
@@ -100,14 +91,12 @@ stdenv.mkDerivation rec {
   buildInputs = [
     dtkwidget
     qt5platform-plugins
-    qt5integration
     deepin-pdfium
     util-dfm
     dde-qt-dbus-factory
     glibmm
     docparser
     dde-dock
-    #deepin-movie-reborn
     qtx11extras
     qtmultimedia
     kcodecs
@@ -135,6 +124,10 @@ stdenv.mkDerivation rec {
   ];
 
   enableParallelBuilding = true;
+
+  qtWrapperArgs = [
+    "--prefix QT_PLUGIN_PATH : ${qt5integration}/${qtbase.qtPluginPrefix}"
+  ];
 
   preFixup = ''
     qtWrapperArgs+=("''${gappsWrapperArgs[@]}")
