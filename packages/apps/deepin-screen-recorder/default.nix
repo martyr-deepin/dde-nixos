f0669803cfaa519de5558caa4d6b0ed352d812ef
rewine <luhongxu@deepin.org>
2023-04-21 13:33:25 +0800

    deepin-screen-recorder: fix

--- packages/apps/deepin-screen-recorder/default.nix
+++ packages/apps/deepin-screen-recorder/default.nix
@@ -1,19 +1,19 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, qmake
+, pkg-config
+, qttools 
+, wrapQtAppsHook
 , dtkwidget
 , qt5integration
 , dde-qt-dbus-factory
 , dde-dock
 , image-editor
 , gsettings-qt
-, qmake
-, qttools
-, pkg-config
 , qtbase
 , qtmultimedia
 , qtx11extras
-, wrapQtAppsHook
 , xorg
 , gst_all_1
 , libusb1
@@ -23,9 +23,7 @@
 , libv4l
 , udev
 , kwayland
-, patchelf
 }:
-
 stdenv.mkDerivation rec {
   pname = "deepin-screen-recorder";
   version = "5.11.23";
@@ -37,9 +35,7 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-yKBF/MmhlgwO5GLwfGgs13ERuzOg8EYjc3bXZ8TvcBU=";
   };
   
-  patches = [
-    ./dont_use_libPath.diff
-  ];
+  patches = [ ./dont_use_libPath.diff ];
 
   postPatch = ''
     substituteInPlace screen_shot_recorder.pro deepin-screen-recorder.desktop \
@@ -68,7 +64,6 @@ stdenv.mkDerivation rec {
     xorg.libXdmcp
     xorg.libXtst
     xorg.libXcursor
-    gst_all_1.gst-plugins-base
     kwayland
     libusb1
     libv4l
@@ -76,14 +71,23 @@ stdenv.mkDerivation rec {
     ffmpegthumbnailer
     portaudio
     udev
-  ];
+  ] ++ (with gst_all_1; [
+    gstreamer
+    gst-plugins-base
+    gst-plugins-good
+  ]);
+
+  cmakeBuildType = "RelWithDebInfo";
 
   # qt5integration must be placed before qtsvg in QT_PLUGIN_PATH
   qtWrapperArgs = [
     "--prefix QT_PLUGIN_PATH : ${qt5integration}/${qtbase.qtPluginPrefix}"
-    "--prefix PATH : ${lib.makeBinPath [ ffmpeg ]}"
-    "--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ udev ffmpeg ]}"
+    "--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath buildInputs}"
   ];
+
+  preFixup = ''
+    qtWrapperArgs+=(--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0")
+  '';
 
   meta = with lib; {
     description = "screen recorder application for dde";
