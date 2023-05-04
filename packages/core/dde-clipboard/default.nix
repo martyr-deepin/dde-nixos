641f65247f20c5b86b27ea19831dc94942e55c4b
rewine <luhongxu@deepin.org>
2023-05-04 17:22:58 +0800

    dde-clipboard: 5.4.25 -> 6.0.4

--- packages/core/dde-clipboard/default.nix
+++ packages/core/dde-clipboard/default.nix
@@ -1,51 +1,50 @@
 { stdenv
 , lib
 , fetchFromGitHub
-, getUsrPatchFrom
 , dtkwidget
 , qt5integration
 , qt5platform-plugins
 , dde-qt-dbus-factory
 , gio-qt
 , cmake
+, extra-cmake-modules
 , qttools
+, wayland
 , kwayland
+, dwayland
 , pkg-config
 , wrapQtAppsHook
 , glibmm
 , gtest
-, qtbase
 }:
-let
-  patchList = {
-    "CMakeLists.txt" = [
-      [ "/etc/xdg" "$out/etc/xdg" ]
-      [ "/lib/systemd/user" "$out/lib/systemd/user" ]
-    ];
-    "misc/dde-clipboard.desktop" = [ ];
-    "misc/dde-clipboard-daemon.service" = [ ];
-    "misc/com.deepin.dde.Clipboard.service" = [
-      [ "/usr/bin/qdbus" "${qttools}/bin/qdbus" ]
-    ];
-  };
-in
+
 stdenv.mkDerivation rec {
   pname = "dde-clipboard";
-  version = "5.4.25";
+  version = "6.0.4";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-oFATOBXf4NvGxjVMlfxwfQkBffeKut8ao+X6T9twb/I=";
+    sha256 = "sha256-FT4gXZJQOkXiAkEGaOsho7OCaamo7TRmnTvcIVLrJKg=";
   };
 
-  postPatch = getUsrPatchFrom patchList + ''
-    patchShebangs translate_generation.sh generate_gtest_report.sh
+  # https://github.com/linuxdeepin/dde-clipboard/pull/132
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace "/etc/xdg" "$out/etc/xdg" \
+      --replace "/lib/systemd/user" "$out/lib/systemd/user" \
+      --replace "/usr/share" "$out/share"
+
+    substituteInPlace misc/{dde-clipboard.desktop,dde-clipboard-daemon.service,org.deepin.dde.Clipboard1.service} \
+      --replace "/usr/bin/qdbus" "${lib.getBin qttools}/bin/qdbus" \
+      --replace "/usr" "$out"
+
   '';
 
   nativeBuildInputs = [
     cmake
+    extra-cmake-modules
     pkg-config
     qttools
     wrapQtAppsHook
@@ -53,23 +52,26 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     dtkwidget
-    dde-qt-dbus-factory
-    gio-qt
-    kwayland
-    glibmm
-    gtest
     qt5integration
     qt5platform-plugins
+    dde-qt-dbus-factory
+    gio-qt
+    wayland
+    kwayland
+    dwayland
+    glibmm
+    gtest
   ];
 
-  cmakeFlags = [
-    "-DUSE_DEEPIN_WAYLAND=OFF"
-  ];
+  # cmakeFlags = [
+  #   "-DUSE_DEEPIN_WAYLAND=OFF"
+  # ];
 
   meta = with lib; {
     description = "DDE optional clipboard manager componment";
     homepage = "https://github.com/linuxdeepin/dde-clipboard";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
+    maintainers = teams.deepin.members;
   };
 }
