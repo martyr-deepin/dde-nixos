b450e39c2892eec09bd0e4516e5780ef4c6c51bc
rewine <luhongxu@deepin.org>
2023-05-06 11:18:16 +0800

    deepin-voice-note: 5.10.22 -> 6.0.6

--- packages/apps/deepin-voice-note/default.nix
+++ packages/apps/deepin-voice-note/default.nix
@@ -1,42 +1,35 @@
 { stdenv
 , lib
 , fetchFromGitHub
-, fetchpatch
+, cmake
+, pkg-config
+, qttools
+, wrapQtAppsHook
+, qtbase
 , dtkwidget
 , qt5integration
 , qt5platform-plugins
 , dde-qt-dbus-factory
-, cmake
-, pkg-config
-, qttools
 , qtmultimedia
 , qtwebengine
-, wrapQtAppsHook
-, wrapGAppsHook
 , libvlc
 , gst_all_1
 , gtest
-, qtbase
 }:
-let
-  gstPluginPath = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with gst_all_1; [ gstreamer gst-plugins-base gst-plugins-good ]);
-in
 stdenv.mkDerivation rec {
   pname = "deepin-voice-note";
-  version = "5.10.22";
+  version = "6.0.6";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-ZDw/kGmhcoTPDUsZa9CYhrVbK4Uo75G0L4q4cCBPr7E=";
+    sha256 = "sha256-h3I5NGr9EwMPCw99wjIJDXe774DeFzi49+cfRvExwDg=";
   };
 
   postPatch = ''
     substituteInPlace src/common/audiowatcher.cpp \
       --replace "/usr/share" "$out/share"
-    substituteInPlace assets/deepin-voice-note.desktop \
-      --replace "/usr/bin" "$out/bin"
   '';
 
   nativeBuildInputs = [
@@ -44,34 +37,39 @@ stdenv.mkDerivation rec {
     pkg-config
     qttools
     wrapQtAppsHook
-    gst_all_1.gstreamer
   ];
 
   buildInputs = [
+    qtbase
     dtkwidget
+    qt5integration
+    qt5platform-plugins
     dde-qt-dbus-factory
     qtmultimedia
     qtwebengine
     libvlc
     gtest
   ] ++ (with gst_all_1; [
+    gstreamer
     gst-plugins-base
     gst-plugins-good
   ]);
 
+  strictDeps = true;
+
   cmakeFlags = [ "-DVERSION=${version}" ];
 
-  NIX_CFLAGS_COMPILE = "-I${dde-qt-dbus-factory}/include/libdframeworkdbus-2.0";
+  env.NIX_CFLAGS_COMPILE = "-I${dde-qt-dbus-factory}/include/libdframeworkdbus-2.0";
 
-  qtWrapperArgs = [
-    "--prefix QT_PLUGIN_PATH : ${qt5integration}/${qtbase.qtPluginPrefix}"
-    "--prefix QT_QPA_PLATFORM_PLUGIN_PATH : ${qt5platform-plugins}/${qtbase.qtPluginPrefix}"
-  ];
+  preFixup = ''
+    qtWrapperArgs+=(--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0")
+  '';
 
   meta = with lib; {
     description = "Simple memo software with texts and voice recordings";
     homepage = "https://github.com/linuxdeepin/deepin-voice-note";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
+    maintainers = teams.deepin.members;
   };
 }
