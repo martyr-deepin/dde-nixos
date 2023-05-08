524ad94f15d564024ba0b872232868df320f8574
rewine <luhongxu@deepin.org>
2023-05-08 11:54:05 +0800

    dde-api: 6.0.7 -> 6.0.6.p7

--- packages/go-package/dde-api/default.nix
+++ packages/go-package/dde-api/default.nix
@@ -26,30 +26,25 @@
 
 buildGoModule rec {
   pname = "dde-api";
-  version = "6.0.7";
+  version = "6.0.6.p7";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
-    rev = "e46a1f75f1902339daf07ab5c50ad1dee05d9c2b";
-    sha256 = "sha256-wnfyo8nXr+c3DRS8oOzQMDIqtuDp+0kw+BFDFasMYvQ=";
+    rev = "e9fd08ffcc41c86b08f4a2fdd88240ad81200191";
+    sha256 = "sha256-OoPpnjpVetLcbgz6cKLdr0o9iWuaBsYel2P34B6xZBQ=";
   };
 
   vendorHash = "sha256-ggcBI8KwvgAQZhAfwCIJaqt7wUAd2lPYYdiJIGetsXo=";
 
-  patches = [ ./0001-fix-PATH-for-NixOS.patch ];
-
   postPatch = ''
-    substituteInPlace lang_info/lang_info.go \
-      --replace "/usr/share/i18n/language_info.json" "${deepin-desktop-base}/share/i18n/language_info.json"
-
     substituteInPlace misc/systemd/system/deepin-shutdown-sound.service \
       --replace "/usr/bin/true" "${coreutils}/bin/true"
 
     substituteInPlace sound-theme-player/main.go \
       --replace "/usr/sbin/alsactl" "alsactl"
 
-    substituteInPlace misc/scripts/deepin-boot-sound.sh \
+    substituteInPlace misc/{scripts/deepin-boot-sound.sh,systemd/system/deepin-login-sound.service} \
      --replace "/usr/bin/dbus-send" "${dbus}/bin/dbus-send"
 
     substituteInPlace lunar-calendar/huangli.go adjust-grub-theme/main.go \
