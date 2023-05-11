b734543115189d590e39caef84f884036c822ff1
rewine <luhongxu@deepin.org>
2023-05-11 11:06:21 +0800

    dde-network-core: 2.0.7 -> 2.0.7.999

--- packages/core/dde-network-core/default.nix
+++ packages/core/dde-network-core/default.nix
@@ -6,40 +6,27 @@
 , qttools
 , pkg-config
 , wrapQtAppsHook
+, qtbase
+, qtsvg
 , dtkwidget
 , dde-dock
 , dde-control-center
 , dde-session-shell
-, dde-qt-dbus-factory
-, gsettings-qt
-, gio-qt
 , networkmanager-qt
 , glib
-, pcre
-, util-linux
-, libselinux
-, libsepol
-, dbus
 , gtest
-, qtbase
 }:
+
 stdenv.mkDerivation rec {
   pname = "dde-network-core";
-  version = "2.0.7";
+  version = "2.0.7.999";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-HTF9NhzgFqQxEDugq/GQiYh50VtGwyyADliTpLN2e7E=";
+    rev = "980db32ffaf916dbde7f775e3a916de6dc820cb0";
+    sha256 = "sha256-oGymFAKCvTG+AxI0EkccxC9n+zc/yPSo2z8uIuGENtc=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/linuxdeepin/dde-network-core/commit/b42d781512d4de0e006ce3beccf16a36b8035f22.patch";
-      sha256 = "sha256-aNnVuxFYlO7t5oONwGeL68dWc6emFZ90OtJO9K/TjzE=";
-    })
-  ];
 
   postPatch = ''
     substituteInPlace dss-network-plugin/notification/bubbletool.cpp \
@@ -54,25 +41,22 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    qtbase
+    qtsvg
     dtkwidget
     dde-dock
     dde-control-center
     dde-session-shell
-    dde-qt-dbus-factory
-    gsettings-qt
-    gio-qt
     networkmanager-qt
     glib
-    pcre
-    util-linux
-    libselinux
-    libsepol
     gtest
   ];
 
   cmakeFlags = [
     "-DVERSION=${version}"
   ];
+
+  strictDeps = true;
 
   meta = with lib; {
     description = "DDE network library framework";
