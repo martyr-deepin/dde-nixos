80ab41aa42e40c4b4f5472e972e69adf69c57911
rewine <luhongxu@deepin.org>
2023-05-11 10:50:50 +0800

    deepin-system-monitor: 5.9.33 -> 6.0.6

--- packages/apps/deepin-system-monitor/default.nix
+++ packages/apps/deepin-system-monitor/default.nix
@@ -21,35 +21,32 @@
 , util-linux
 , systemd
 , polkit
+, wayland
+, dwayland
 }:
 
 stdenv.mkDerivation rec {
   pname = "deepin-system-monitor";
-  version = "5.9.33";
+  version = "6.0.6";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-X7/YwnJyA/HOLsOGARjsHWgL2qxW1eU1TvoWulvz0j4=";
+    sha256 = "sha256-4GViTW0Yho2uiLdsb6LYswZL2GQGWlM1W9G+mfrbNdo=";
   };
 
   postPatch = ''
-    substituteInPlace deepin-system-monitor-main/process/process_controller.cpp \
-      deepin-system-monitor-main/process/priority_controller.cpp \
-      deepin-system-monitor-main/service/service_manager.cpp \
-      deepin-system-monitor-main/translations/policy/com.deepin.pkexec.deepin-system-monitor.policy \
-        --replace "/usr/bin/kill" "${util-linux}/bin/kill" \
-        --replace "/usr/bin/renice" "${util-linux}/bin/renice" \
-        --replace '/usr/bin/systemctl' '${lib.getBin systemd}/systemctl'
-
     substituteInPlace deepin-system-monitor-main/{service/service_manager.cpp,process/{priority_controller.cpp,process_controller.cpp}} \
-      --replace "/usr/bin/pkexec" "${lib.getBin polkit}/bin/pkexec"
+      deepin-system-monitor-main/translations/policy/com.deepin.pkexec.deepin-system-monitor.policy \
+      --replace "/usr/bin/pkexec" "${lib.getBin polkit}/bin/pkexec" \
+      --replace "/usr/bin/kill" "${util-linux}/bin/kill" \
+      --replace "/usr/bin/renice" "${util-linux}/bin/renice" \
+      --replace '/usr/bin/systemctl' '${lib.getBin systemd}/systemctl'
 
     for file in $(grep -rl "/usr")
     do
-      substituteInPlace $file \
-        --replace "/usr" "$out"
+      substituteInPlace $file --replace "/usr" "$out"
     done
   '';
 
@@ -74,11 +71,12 @@ stdenv.mkDerivation rec {
     procps
     libpcap
     libnl
+    wayland
+    dwayland
   ];
 
   cmakeFlags = [
     "-DVERSION=${version}"
-    "-DUSE_DEEPIN_WAYLAND=OFF"
   ];
 
   strictDeps = true;
