76bea2c7c832843aa16f4518da73dc2f44472861
rewine <luhongxu@deepin.org>
2023-05-15 16:03:01 +0800

    dde-account-faces: 1.0.12.1 -> 1.0.14

--- packages/artwork/dde-account-faces/default.nix
+++ packages/artwork/dde-account-faces/default.nix
@@ -5,13 +5,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "dde-account-faces";
-  version = "1.0.12.1";
+  version = "1.0.14";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    sha256 = "sha256-NWjR8qxWi2IrcP0cSF+lLxBJ/GrVpk1BfTjVH0ytinY=";
+    sha256 = "sha256-N/a29V3SbwbSW6IZrSunGqlaudHBp9AiCtmampIZ8Pw=";
   };
 
   makeFlags = [ "PREFIX=${placeholder "out"}/var" ];
