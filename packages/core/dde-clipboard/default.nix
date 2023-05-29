f24ce989ab957220defe8a7826ff58b519687666
rewine <luhongxu@deepin.org>
2023-05-29 11:39:03 +0800

    dde-clipboard: 6.0.4 -> 6.0.4.999

--- packages/core/dde-clipboard/default.nix
+++ packages/core/dde-clipboard/default.nix
@@ -1,10 +1,8 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, fetchpatch
 , dtkwidget
-, qt5integration
-, qt5platform-plugins
-, dde-qt-dbus-factory
 , gio-qt
 , cmake
 , extra-cmake-modules
@@ -20,26 +18,26 @@
 
 stdenv.mkDerivation rec {
   pname = "dde-clipboard";
-  version = "6.0.4";
+  version = "6.0.4.999";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-FT4gXZJQOkXiAkEGaOsho7OCaamo7TRmnTvcIVLrJKg=";
+    rev = "7db6a31d63e27057712b485e7d266f90f0d58d8e";
+    hash = "sha256-2AV+zkCk0R1vcK7E0OQ9dIqog5ViOfMQ3xx/0DFE6FM=";
   };
 
-  # https://github.com/linuxdeepin/dde-clipboard/pull/132
-  postPatch = ''
-    substituteInPlace CMakeLists.txt \
-      --replace "/etc/xdg" "$out/etc/xdg" \
-      --replace "/lib/systemd/user" "$out/lib/systemd/user" \
-      --replace "/usr/share" "$out/share"
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/linuxdeepin/dde-clipboard/pull/134/commits/adbacff5943fe4f1ffadd984e6555eb427676108.patch";
+      sha256 = "sha256-uQB3I52+BqBv9nGNW/mhBcs5iroBVXrUjgJxxxwtGu0=";
+    })
+  ];
 
+  postPatch = ''
     substituteInPlace misc/{dde-clipboard.desktop,dde-clipboard-daemon.service,org.deepin.dde.Clipboard1.service} \
       --replace "/usr/bin/qdbus" "${lib.getBin qttools}/bin/qdbus" \
       --replace "/usr" "$out"
-
   '';
 
   nativeBuildInputs = [
@@ -52,9 +50,6 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     dtkwidget
-    qt5integration
-    qt5platform-plugins
-    dde-qt-dbus-factory
     gio-qt
     wayland
     kwayland
@@ -62,10 +57,6 @@ stdenv.mkDerivation rec {
     glibmm
     gtest
   ];
-
-  # cmakeFlags = [
-  #   "-DUSE_DEEPIN_WAYLAND=OFF"
-  # ];
 
   meta = with lib; {
     description = "DDE optional clipboard manager componment";
