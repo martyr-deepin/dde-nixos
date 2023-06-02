1e2e966efcd3d61a9032b6c58d67136abbfafc61
rewine <luhongxu@deepin.org>
2023-06-02 22:27:58 +0800

    deepin-screen-recorder: fix libv4l

--- packages/apps/deepin-screen-recorder/default.nix
+++ packages/apps/deepin-screen-recorder/default.nix
@@ -81,7 +81,7 @@ stdenv.mkDerivation rec {
   # qt5integration must be placed before qtsvg in QT_PLUGIN_PATH
   qtWrapperArgs = [
     "--prefix QT_PLUGIN_PATH : ${qt5integration}/${qtbase.qtPluginPrefix}"
-    "--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ udev gst_all_1.gstreamer ]}"
+    "--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ udev gst_all_1.gstreamer libv4l ]}"
   ];
 
   preFixup = ''
