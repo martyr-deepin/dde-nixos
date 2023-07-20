6f72cd24c6e0b65582b16356ff9ed388bd6dbadc
rewine <luhongxu@deepin.org>
2023-07-20 14:22:02 +0800

    dde-application-manager: 1.0.17 -> 1.0.19

--- packages/core/dde-application-manager/default.nix
+++ packages/core/dde-application-manager/default.nix
@@ -16,20 +16,18 @@
 
 stdenv.mkDerivation rec {
   pname = "dde-application-manager";
-  version = "1.0.17";
+  version = "1.0.19";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    hash = "sha256-3GOt4R5EG5OELyFv+YJ3TyKmjYq6I9ZOA0AbBtL5IrU=";
+    hash = "sha256-1P265xqlL/wML66nKdfTgkRx6MCpLwrt5rXu+CyeShU=";
   };
 
   ## TODO
   postPatch = ''
     substituteInPlace src/modules/mimeapp/mime_app.cpp src/modules/launcher/common.h src/service/main.cpp \
-      src/modules/dock/common.h \
-      misc/dconf/com.deepin.dde.dock.json \
       misc/dconf/com.deepin.dde.appearance.json \
       --replace "/usr/share" "/run/current-system/sw/share"
 
