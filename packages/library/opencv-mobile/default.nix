f13d67504bef01fad7adcd9d318838b45784ef0a
rewine <luhongxu@deepin.org>
2023-08-05 16:19:36 +0800

    opencv-mobile: fix

--- packages/library/opencv-mobile/default.nix
+++ packages/library/opencv-mobile/default.nix
@@ -1,44 +1,62 @@
 { stdenv
 , lib
 , fetchFromGitHub
-, pkg-config
 , cmake
-, qttools
-, wrapQtAppsHook
-, libisoburn
 }:
 
+let
+  patchver = "16";
+  
+  opencv4 = fetchFromGitHub {
+      owner = "opencv";
+      repo = "opencv";
+      rev = "4.6.0";
+      hash = "sha256-zPkMc6xEDZU5TlBH3LAzvB17XgocSPeHVMG/U6kfpxg=";
+  };
+
+  patchSrc = fetchFromGitHub {
+      owner = "nihui";
+      repo = "opencv-mobile";
+      rev = "v${patchver}";
+      hash = "sha256-ijt3EoezUr9Pnh0FFHL7y1Or/ec63sgKds1p4Ob5Tcc=";
+  };
+in
 stdenv.mkDerivation rec {
   pname = "opencv-mobile";
-  version = "0.0.1.999";
+  version = "4.6-16";
 
-  src = fetchFromGitHub {
-    owner = "deepin";
-    repo = pname;
-    rev = "33c55b103f704fe329a1c93379bf99a6bddbb0f6";
-    sha256 = "";
-  };
+  src = opencv4;
 
   nativeBuildInputs = [
     cmake
-    # qttools
-    # pkg-config
-    # wrapQtAppsHook
   ];
 
-  #buildInputs = [ libisoburn ];
   postPatch = ''
-    patch -p1 -i opencv-4.6.0-no-zlib.patch
+    patch -p1 -i ${patchSrc}/opencv-4.6.0-no-zlib.patch
     truncate -s 0 cmake/OpenCVFindLibsGrfmt.cmake
     rm -rf modules/gapi
     rm -rf modules/highgui
-    cp -r ../highgui modules/
-    patch -p1 -i ../opencv-4.6.0-no-rtti.patch
+    cp -r ${patchSrc}/highgui modules/
+    chmod +w modules/highgui
+    patch -p1 -i ${patchSrc}/opencv-4.6.0-no-rtti.patch
   '';
 
-  cmakeFlags = [
-    "`cat ${src}/opencv4_cmake_options.txt`"
-  ];
+  configurePhase = ''
+    runHook preBuild
+    mkdir -p build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=$out \
+-DCMAKE_BUILD_TYPE=Release \
+`cat ${patchSrc}/opencv4_cmake_options.txt` \
+-DBUILD_opencv_world=OFF ..
+    runHook postBuild
+  '';
+
+
+  #cmakeFlags = [
+  #  "`cat ${patchSrc}/opencv4_cmake_options.txt`"
+  #  "-DBUILD_opencv_world=OFF"
+  #];
 
   meta = with lib; {
     description = "The minimal opencv for Android, iOS, ARM Linux, Windows, Linux, MacOS, WebAssembly";
