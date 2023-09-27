b72c78f80469498c04b1d56b9c923286b43aea52
rewine <luhongxu@deepin.org>
2023-09-27 10:29:09 +0800

    deepin-voice-note: fix build

--- packages/apps/deepin-voice-note/default.nix
+++ packages/apps/deepin-voice-note/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, fetchpatch
 , cmake
 , pkg-config
 , qttools
@@ -16,6 +17,7 @@
 , gst_all_1
 , gtest
 }:
+
 stdenv.mkDerivation rec {
   pname = "deepin-voice-note";
   version = "6.0.13";
@@ -27,7 +29,18 @@ stdenv.mkDerivation rec {
     hash = "sha256-yDlWyMGkSToGCN7tuZNR8Mz7MUOZ7355w4H0OzeHBrs=";
   };
 
-  patches = [ 
+  patches = [
+    ./use_v23_dbus_interface.diff
+    (fetchpatch {
+      name = "Adjust-the-audio-port-available-range.patch";
+      url = "https://github.com/linuxdeepin/deepin-voice-note/commit/a876e4c4cf7d77e50071246f9fb6998aa62def77.patch";
+      hash = "sha256-J/PPdj1Am/v2Sw2Dv2XvZJAy/6Tf7OoTfrbOB9rc5m8=";
+    })
+    (fetchpatch {
+      name = "fix-build-error-with-new-dtk.patch";
+      url = "https://github.com/linuxdeepin/deepin-voice-note/commit/9ce211f603deaff21b881e1c4f43d53e33a85347.patch";
+      hash = "sha256-oP+AzMniONxjYIFust8fGaD8/UOjKr4yZiRUkdTMd5w=";
+    })
     "${src}/patches/use-cmake-variable-if-define.patch"
     "${src}/patches/fix-create-vnote-db-failed.patch"
   ];
@@ -63,8 +76,6 @@ stdenv.mkDerivation rec {
   strictDeps = true;
 
   cmakeFlags = [ "-DVERSION=${version}" ];
-
-  env.NIX_CFLAGS_COMPILE = "-I${dde-qt-dbus-factory}/include/libdframeworkdbus-2.0";
 
   preFixup = ''
     qtWrapperArgs+=(--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0")
